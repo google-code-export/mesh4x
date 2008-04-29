48af5e105c87f76dd471506d4923b72741d6ba60
jtondato@gmail.com <jtondato@gmail.com@8593f39d-8f4b-0410-bdb6-4f53e344ee92>
2008-04-29 21:39:23 +0000

    base line 0.

---
+++ Mesh4j/trunk/Mesh4j/src/com/mesh4j/sync/observer/ItemObservable.java
@@ -0,0 +1,32 @@
+package com.mesh4j.sync.observer;
+
+import java.util.Vector;
+
+import com.mesh4j.sync.model.Item;
+
+public class ItemObservable {
+	private Vector<ItemObserver> observers = new Vector<ItemObserver>();
+
+	public void notifyObservers(Item item) {
+		ItemObserver[] arrLocal;
+		synchronized (this) {
+			arrLocal = observers.toArray(new ItemObserver[0]);
+			for (int i = arrLocal.length - 1; i >= 0; i--){
+				(arrLocal[i]).notifyItemNovelty(item);
+			}
+		}
+	}
+
+	public synchronized void addObserver(ItemObserver observer) {
+		if (observer == null)
+			throw new NullPointerException();
+		if (!observers.contains(observer)) {
+			observers.addElement(observer);
+		}
+	}
+
+	public synchronized void removeObserver(ItemObserver observer) {
+		observers.removeElement(observer);
+	}
+
+}
