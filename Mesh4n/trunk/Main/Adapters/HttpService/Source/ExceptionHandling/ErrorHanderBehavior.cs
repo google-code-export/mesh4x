104be70fdaefd0a4a52c7526e3f2230a6a588b1e
cibrax <cibrax@8593f39d-8f4b-0410-bdb6-4f53e344ee92>
2008-05-09 19:17:58 +0000

    Added missing files

---
+++ Mesh4n/trunk/Main/Adapters/HttpService/Source/ExceptionHandling/ErrorHanderBehavior.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ServiceModel;
+using System.ServiceModel.Channels;
+using System.ServiceModel.Dispatcher;
+using System.ServiceModel.Description;
+using System.ServiceModel.Web;
+
+namespace Mesh4n.Adapters.HttpService.ExceptionHandling
+{
+    public class ErrorHanderBehavior: WebHttpBehavior
+    {
+		IErrorHandler errorHandler;
+
+		public ErrorHanderBehavior(IErrorHandler errorHandler) 
+        { 
+            this.errorHandler = errorHandler; 
+        }
+
+        protected override void AddServerErrorHandlers(ServiceEndpoint endpoint, EndpointDispatcher endpointDispatcher)  
+        {
+            endpointDispatcher.ChannelDispatcher.ErrorHandlers.Clear();  
+            endpointDispatcher.ChannelDispatcher.ErrorHandlers.Add(errorHandler);  
+        }
+    }
+}
