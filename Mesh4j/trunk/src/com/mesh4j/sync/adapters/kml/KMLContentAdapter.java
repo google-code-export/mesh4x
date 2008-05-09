cbabde7148bc0a4df590ab27083cc7d7869e1c8d
jtondato@gmail.com <jtondato@gmail.com@8593f39d-8f4b-0410-bdb6-4f53e344ee92>
2008-05-09 19:45:49 +0000

    Style sharing.

--- Mesh4j/trunk/src/com/mesh4j/sync/adapters/kml/KMLContentAdapter.java
+++ Mesh4j/trunk/src/com/mesh4j/sync/adapters/kml/KMLContentAdapter.java
@@ -21,7 +21,6 @@ import com.mesh4j.sync.adapters.EntityContent;
 import com.mesh4j.sync.adapters.compound.IContentAdapter;
 import com.mesh4j.sync.model.IContent;
 import com.mesh4j.sync.utils.IdGenerator;
-import com.mesh4j.sync.utils.StringHelper;
 import com.mesh4j.sync.utils.XMLHelper;
 
 public class KMLContentAdapter implements IContentAdapter{
@@ -39,8 +38,6 @@ public class KMLContentAdapter implements IContentAdapter{
 	private static final String SHARED_ITEMS = "Shared Items";
 	private static final String KML_ELEMENT_DOCUMENT = "Document";
 	private static final String KML_ELEMENT_PLACEMARK = "Placemark";
-	private static final String KML_ELEMENT_STYLE_MAP ="StyleMap";
-	private static final String KML_ELEMENT_STYLE ="Style";
 	
 	private static final Namespace XML_NS = DocumentHelper.createNamespace(XML_PREFIX, XML_URI);
 	private static final QName XML_ID_QNAME = DocumentHelper.createQName("id", XML_NS);
@@ -101,56 +98,16 @@ public class KMLContentAdapter implements IContentAdapter{
 			if(KML_ELEMENT_PLACEMARK.equals(newPayload.getName())){
 				this.kmlSharedFolderElement.add(newPayload);	
 			} else {
-				this.insertAfterFirst(KML_ELEMENT_NAME, newPayload);
+				this.kmlDocumentElement.add(newPayload);
 			}
 		} else {
-			if(KML_ELEMENT_PLACEMARK.equals(newPayload.getName())){
-				Element parent = element.getParent();
-				parent.remove(element);
-				parent.add(newPayload);
-			} else {
-				this.replaceElement(element, newPayload);
-			}
+			Element parent = element.getParent();
+			parent.remove(element);
+			parent.add(newPayload);
 		}
 		this.flush();
 	}
-
-	@SuppressWarnings("unchecked")
-	private void replaceElement(Element originalElement, Element newElement) {
-		Element kmlElement = this.kmlDocument.getRootElement();
-		Element newDocumentElement = kmlElement.addElement(KML_ELEMENT_DOCUMENT, KML_URI);
-		
-		List<Element> elements = this.kmlDocumentElement.elements();
-		for (Element element : elements) {
-			if(element == originalElement){
-				newDocumentElement.add(newElement);
-			} else {
-				newDocumentElement.add(element.createCopy());
-			}
-		}
-		kmlElement.remove(this.kmlDocumentElement);
-		this.kmlDocumentElement = newDocumentElement;
-	}
-	
-	@SuppressWarnings("unchecked")
-	private void insertAfterFirst(String elementName, Element newPayload) {
-		Element kmlElement = this.kmlDocument.getRootElement();
-		Element newDocumentElement = kmlElement.addElement(KML_ELEMENT_DOCUMENT, KML_URI);
-		
-		List<Element> elements = this.kmlDocumentElement.elements();
-		boolean mustAdd = true;
-		for (Element element : elements) {
-			newDocumentElement.add(element.createCopy());
-			if(elementName.equals(element.getName()) && mustAdd){
-				newDocumentElement.add(newPayload);
-				mustAdd = false;
-			}
-		}
-		kmlElement.remove(this.kmlDocumentElement);
-		this.kmlDocumentElement = newDocumentElement;
-		
-	}
-
+ 	
 	private Element getElementById(String entityId)  {
 		HashMap<String, String> map = new HashMap<String, String>();
 		map.put(XML_PREFIX, XML_URI);
@@ -190,11 +147,10 @@ public class KMLContentAdapter implements IContentAdapter{
 	public List<EntityContent> getAll() {
 		
 		ArrayList<EntityContent> result = new ArrayList<EntityContent>();
-		ArrayList<String> syncIds = new ArrayList<String>();
 		try{	
 			boolean dirty = false;
-			List<Element> elements = this.selectElements("//kml:Placemark");
-			
+
+			List<Element> elements = this.selectElements("//kml:Placemark");			
 			for (Element element : elements) {
 				String id = element.attributeValue(XML_ID_QNAME);
 				if(id == null){
@@ -205,71 +161,35 @@ public class KMLContentAdapter implements IContentAdapter{
 				}
 				EntityContent entityContent = new EntityContent(element, this.getEntityName(), id);
 				result.add(entityContent);
-				syncIds.add(id);
 			}
 			
 			elements = this.selectElements("//kml:Style");
-			
-			HashMap<String, String> styleIdsToReplace = new HashMap<String, String>();
+			elements.addAll(this.selectElements("//kml:StyleMap"));			
 			for (Element element : elements) {
 				String id = element.attributeValue(XML_ID_QNAME);
 				if(id == null){
 					id = makeNewID();
 					element.addNamespace(XML_PREFIX, XML_URI);
 					element.addAttribute(XML_ID_QNAME, id);
-					dirty = true;
+					
 					String kmlID = element.attributeValue("id");
-					styleIdsToReplace.put(kmlID, kmlID+"_"+id);
+					element.addAttribute("id", kmlID+"_"+id);
+					
+					String kmlIDRef = "#"+kmlID;
+					String newKmlIDRef = "#"+kmlID+"_"+id;
+					
+					List<Element> references = this.selectElements("//kml:styleUrl[text()='"+ kmlIDRef +"']");
+					for (Element refElement : references) {
+						refElement.setText(newKmlIDRef);
+					}
+					dirty = true;
 				}
 				EntityContent entityContent = new EntityContent(element, this.getEntityName(), id);
 				result.add(entityContent);
-				syncIds.add(id);
-			}
-	
-			elements = this.selectElements("//kml:StyleMap");
-			
-			HashMap<String, String> styleMapIdsToReplace = new HashMap<String, String>();
-			for (Element element : elements) {
-				String id = element.attributeValue(XML_ID_QNAME);
-				if(id == null){
-					id = makeNewID();
-					element.addNamespace(XML_PREFIX, XML_URI);
-					element.addAttribute(XML_ID_QNAME, id);
-					dirty = true;
-					String kmlID = element.attributeValue("id");
-					styleMapIdsToReplace.put(kmlID, kmlID+"_"+id);
-				}
-				EntityContent entityContent = new EntityContent(element, this.getEntityName(), id);
-				result.add(entityContent);
-				syncIds.add(id);
 			}
 			
 			if(dirty){
-				if(!styleMapIdsToReplace.isEmpty() || !styleIdsToReplace.isEmpty()){
-					String xml = this.kmlDocument.asXML();
-					
-					for (String kmlID : styleMapIdsToReplace.keySet()) {
-						String newID = styleMapIdsToReplace.get(kmlID);
-						xml = StringHelper.replace(xml, kmlID, newID);
-					}
-					for (String kmlID : styleIdsToReplace.keySet()) {
-						String newID = styleIdsToReplace.get(kmlID);
-						xml = StringHelper.replace(xml, kmlID, newID);
-					}					
-					
-					this.kmlDocument = DocumentHelper.parseText(xml);
-					this.initializeDocumentElement();
-					this.initializeFolderElement();
-				}
 				this.flush();
-				
-				result = new ArrayList<EntityContent>();
-				
-				for (String syncID : syncIds) {
-					Element element = this.getElementById(syncID);
-					EntityContent entityContent = new EntityContent(element, this.getEntityName(), syncID);
-					result.add(entityContent);
-				}
 			}
 		} catch (Exception e) {
 			Logger.error(e.getMessage(), e);
