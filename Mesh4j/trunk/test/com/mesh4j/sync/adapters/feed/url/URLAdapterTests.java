3183d5e3d02ca1e23adfda6d76aed55f65a9a0d3
jtondato@gmail.com <jtondato@gmail.com@8593f39d-8f4b-0410-bdb6-4f53e344ee92>
2008-05-12 17:40:27 +0000

    URLFeedAdapter

--- Mesh4j/trunk/test/com/mesh4j/sync/adapters/feed/url/URLAdapterTests.java
+++ Mesh4j/trunk/test/com/mesh4j/sync/adapters/feed/url/URLAdapterTests.java
@@ -1,4 +1,4 @@
-package com.mesh4j.sync.adapters.url;
+package com.mesh4j.sync.adapters.feed.url;
 
 import java.net.MalformedURLException;
 import java.util.List;
@@ -9,6 +9,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 import com.mesh4j.sync.adapters.feed.rss.RssSyndicationFormat;
+import com.mesh4j.sync.adapters.feed.url.URLFeedAdapter;
 import com.mesh4j.sync.model.Item;
 import com.mesh4j.sync.security.NullSecurity;
 import com.mesh4j.sync.test.utils.TestHelper;
@@ -17,21 +18,28 @@ import com.mesh4j.sync.test.utils.TestHelper;
 public class URLAdapterTests {
 
 	// MODEL VARIABLES
-	private URLAdapter urlAdapter;
+	private URLFeedAdapter urlAdapter;
 	
 	// BUSINESS METHODS
 	
 	@Before
 	public void setUp() throws MalformedURLException{
-		String path = "http://pci-mobile:7777/feeds/MockFeed";
-		this.urlAdapter = new URLAdapter(path, RssSyndicationFormat.INSTANCE, NullSecurity.INSTANCE);
+		String path = "http://localhost:7777/feeds/MockFeed";
+		this.urlAdapter = new URLFeedAdapter(path, RssSyndicationFormat.INSTANCE, NullSecurity.INSTANCE);
 	}
 		
-	//@Test
+	@Test
 	public void shouldExecuteGetAll(){
 		List<Item> items = this.urlAdapter.getAll();
 		Assert.assertEquals(1, items.size());
 	}
+	
+	@Test
+	public void shouldExecuteGetAllSince(){
+		List<Item> items = this.urlAdapter.getAllSince(TestHelper.now());
+		Assert.assertEquals(1, items.size());
+	}
+
 	
 	@Test
 	public void shouldExecuteMerge(){
@@ -43,7 +51,7 @@ public class URLAdapterTests {
 		item.getSync().update("jmt", TestHelper.now());
 		
 		List<Item> result = this.urlAdapter.merge(items);
-		Assert.assertEquals(0, items.size());
+		Assert.assertEquals(0, result.size());
 	}
 	
 	// TODO (JMT) test
