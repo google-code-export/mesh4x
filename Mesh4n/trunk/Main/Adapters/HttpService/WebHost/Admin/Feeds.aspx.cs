85f11860d27db7cc041ddd9ee1a5205c9be76ffa
cibrax <cibrax@8593f39d-8f4b-0410-bdb6-4f53e344ee92>
2008-05-13 21:55:35 +0000

    Added aspx page to show feeds in an specific output format (Rss, KML and KML network)

---
+++ Mesh4n/trunk/Main/Adapters/HttpService/WebHost/Admin/Feeds.aspx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Xml.Linq;
+
+public partial class Admin_Feeds : System.Web.UI.Page
+{
+	protected void Page_Load(object sender, EventArgs e)
+	{
+		string feedName = Request.QueryString["feed"];
+
+		lnkRssFeed.NavigateUrl = string.Format(lnkRssFeed.NavigateUrl, feedName);
+		lnkKmlFeed.NavigateUrl = string.Format(lnkKmlFeed.NavigateUrl, feedName);
+		lnkKmlNetworkFeed.NavigateUrl = string.Format(lnkKmlNetworkFeed.NavigateUrl, feedName);
+	}
+}
