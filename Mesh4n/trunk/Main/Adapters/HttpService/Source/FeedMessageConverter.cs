8405596368f15881d089794d4c44073bbc6f0801
cibrax <cibrax@8593f39d-8f4b-0410-bdb6-4f53e344ee92>
2008-05-13 20:37:28 +0000

    Added new interface IMessageFormatter.cs to generate different output formats in the Http Service

---
+++ Mesh4n/trunk/Main/Adapters/HttpService/Source/FeedMessageConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Mesh4n.Adapters.HttpService
+{
+	public class FeedMessageConverter : IMessageFormatter
+	{
+		#region IMessageFormatter Members
+
+		public System.ServiceModel.Channels.Message Format(Feed feed, IEnumerable<Item> items, System.ServiceModel.Web.IWebOperationContext context)
+		{
+			throw new NotImplementedException();
+		}
+
+		#endregion
+	}
+}
