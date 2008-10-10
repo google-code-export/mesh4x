b320368ef3efd8dc188235e343ab679acf8e0e8b
jtondato@gmail.com <jtondato@gmail.com@8593f39d-8f4b-0410-bdb6-4f53e344ee92>
2008-10-10 17:44:51 +0000

    Release: mesh4j-SMS-DemoApp-0.1.9

--- Mesh4j/trunk/src/org/mesh4j/sync/message/core/repository/IEndpointFactory.java
+++ Mesh4j/trunk/src/org/mesh4j/sync/message/core/repository/IEndpointFactory.java
@@ -3,7 +3,5 @@ package org.mesh4j.sync.message.core.repository;
 import org.mesh4j.sync.message.IEndpoint;
 
 public interface IEndpointFactory {
-
-	public IEndpoint makeIEndpoint(String endpoint);
-	
+	public IEndpoint makeIEndpoint(String endpoint);	
 }
