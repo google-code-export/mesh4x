37b85d6d77b33ffc17c4646bccaef5578d0efa6f
jtondato@gmail.com <jtondato@gmail.com@8593f39d-8f4b-0410-bdb6-4f53e344ee92>
2008-12-18 15:13:21 +0000

    Epiinfo-Sms bug fix and demo last changes.

---
+++ Mesh4j/trunk/src/org/mesh4j/sync/message/protocol/EqualStatusMessageProcessor.java
@@ -0,0 +1,54 @@
+package org.mesh4j.sync.message.protocol;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.mesh4j.sync.message.IMessage;
+import org.mesh4j.sync.message.IMessageSyncProtocol;
+import org.mesh4j.sync.message.ISyncSession;
+import org.mesh4j.sync.message.core.IMessageProcessor;
+import org.mesh4j.sync.message.core.Message;
+import org.mesh4j.sync.validations.Guard;
+
+public class EqualStatusMessageProcessor implements IMessageProcessor{
+
+	public static final String MESSAGE_TYPE = "E";
+	
+	// MODEL VARIABLES
+	private EndSyncMessageProcessor endMessage;
+	
+	// METHODS
+	public EqualStatusMessageProcessor(EndSyncMessageProcessor endMessage) {
+		super();
+		this.endMessage = endMessage;
+	}
+	
+	@Override
+	public String getMessageType() {
+		return MESSAGE_TYPE;
+	}
+	
+	public IMessage createMessage(ISyncSession syncSession) {
+		Guard.argumentNotNull(syncSession, "syncSession");
+				
+		return new Message(
+				IProtocolConstants.PROTOCOL,
+				this.getMessageType(),
+				syncSession.getSessionId(),
+				syncSession.getVersion(),
+				"",
+				syncSession.getTarget());
+	}
+
+
+	@Override
+	public List<IMessage> process(ISyncSession syncSession, IMessage message) {
+		if(syncSession.isOpen() && syncSession.getVersion() == message.getSessionVersion() && this.getMessageType().equals(message.getMessageType())){
+			ArrayList<IMessage> response = new ArrayList<IMessage>();
+			response.add(this.endMessage.createMessage(syncSession));
+			return response;
+		}
+		return IMessageSyncProtocol.NO_RESPONSE;
+	}
+
+}
