05eb7998dab9fd8ac6c4e91b66a3e3053ceecea2
jtondato@gmail.com <jtondato@gmail.com@8593f39d-8f4b-0410-bdb6-4f53e344ee92>
2009-02-11 14:05:54 +0000

    Issue 94: Epiinfo UI Redesign - lock and feel reviews

--- Mesh4j/trunk/Mesh4jEpiInfo/src/org/mesh4j/sync/ui/tasks/OpenFileTask.java
+++ Mesh4j/trunk/Mesh4jEpiInfo/src/org/mesh4j/sync/ui/tasks/OpenFileTask.java
@@ -1,30 +1,41 @@
 package org.mesh4j.sync.ui.tasks;
 
+import java.awt.Cursor;
 import java.awt.Desktop;
 import java.io.File;
 import java.io.IOException;
 
+import javax.swing.JFrame;
 import javax.swing.SwingWorker;
 
 import org.mesh4j.sync.ui.LogFrame;
+import org.mesh4j.sync.ui.translator.MeshCompactUITranslator;
 
 public class OpenFileTask extends SwingWorker<Void, Void> {
 	 
 	// MODEL VARIABLEs
 	private String fileName;
+	private IErrorListener errorListener;
+	private JFrame frame;
 	
 	// BUSINESS METHODS
-	public OpenFileTask(String fileName){
+	public OpenFileTask(JFrame frame, IErrorListener errorListener, String fileName){
 		super();
 		this.fileName = fileName;
+		this.errorListener = errorListener;
+		this.frame = frame;
 	}
 	
 	@Override
     public Void doInBackground() {
+		this.frame.setCursor(Cursor.getPredefinedCursor(Cursor.WAIT_CURSOR));
+		
 		try{
 			File file = new File(fileName);
 			if(file.exists()){
-				openFile(file);
+				openFile(this.errorListener, file);
+			} else {
+				errorListener.notifyError(MeshCompactUITranslator.getErrorImpossibleToOpenFileBecauseFileDoesNotExists());
 			}
 		}catch(Throwable e){
 			LogFrame.Logger.error(e.getMessage(), e);
@@ -32,16 +43,29 @@ public class OpenFileTask extends SwingWorker<Void, Void> {
 		return null;
     }
 	
-	public static void openFile(String fileName) throws IOException {
-		openFile(new File(fileName));
+	public static void openFile(IErrorListener errorListener, String fileName) throws IOException {
+		openFile(errorListener, new File(fileName));
 	}
 	
-	public static void openFile(File file) throws IOException {
+	public static void openFile(IErrorListener errorListener, File file) throws IOException {
 		if (Desktop.isDesktopSupported()) {
 			Desktop desktop = Desktop.getDesktop();
 			if (desktop.isSupported(Desktop.Action.OPEN)) {
-				desktop.open(file.getCanonicalFile());
+				if(file.exists()){
+					desktop.open(file.getCanonicalFile());
+				} else {
+					errorListener.notifyError(MeshCompactUITranslator.getErrorImpossibleToOpenFileBecauseFileDoesNotExists());
+				}
+			} else {
+				errorListener.notifyError(MeshCompactUITranslator.getErrorOpenFileActionNotSupported());
 			}
+		} else {
+			errorListener.notifyError(MeshCompactUITranslator.getErrorOpenFileActionNotSupported());	
 		}
 	}
+	
+	@Override
+    public void done() {
+		this.frame.setCursor(Cursor.getPredefinedCursor(Cursor.DEFAULT_CURSOR));
+    }
 }
