57dac6ac28c398c311c87788bdacf8b321e70a30
auni.ku@gmail.com <auni.ku@gmail.com@8593f39d-8f4b-0410-bdb6-4f53e344ee92>
2009-03-25 12:59:11 +0000

    code added for batch operation

--- Mesh4j/spikes/GrameenTraining/test/org/mesh4j/grameen/training/intro/adapter/googlespreadsheet/GoogleSpreadsheetUtilsTests.java
+++ Mesh4j/spikes/GrameenTraining/test/org/mesh4j/grameen/training/intro/adapter/googlespreadsheet/GoogleSpreadsheetUtilsTests.java
@@ -1,23 +1,20 @@
 package org.mesh4j.grameen.training.intro.adapter.googlespreadsheet;
 
-import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
 import junit.framework.Assert;
 
-import org.apache.poi.hssf.usermodel.HSSFRow;
-import org.apache.poi.hssf.usermodel.HSSFSheet;
-import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.junit.Before;
 import org.junit.Test;
 import org.mesh4j.grameen.training.intro.adapter.googlespreadsheet.GoogleSpreadsheet;
 import org.mesh4j.grameen.training.intro.adapter.googlespreadsheet.GoogleSpreadsheetUtils;
 import org.mesh4j.grameen.training.intro.adapter.googlespreadsheet.MJCellEntry;
 import org.mesh4j.grameen.training.intro.adapter.googlespreadsheet.MJListEntry;
-import org.mesh4j.sync.model.Item;
-import org.mesh4j.sync.test.utils.TestHelper;
 
+
+import com.google.gdata.data.batch.BatchOperationType;
+import com.google.gdata.data.batch.BatchUtils;
 import com.google.gdata.data.spreadsheet.ListEntry;
 import com.google.gdata.data.spreadsheet.SpreadsheetEntry;
 import com.google.gdata.data.spreadsheet.WorksheetEntry;
@@ -37,14 +34,13 @@ public class GoogleSpreadsheetUtilsTests {
 				username, password);
 	}
 	
-	//@Test
+
 	public void shouldLoadSpreadsheetWhenFileExist()
 			throws FileNotFoundException, IOException {		
 		SpreadsheetEntry sse = getSampleGoogleSpreadsheet();
 		// GoogleSpreadsheetUtils.flush(sse, spreadsheetFileId);
 		Assert.assertNotNull(sse);
 	}
-
 	
 	public void shouldGetRow(){
 		SpreadsheetEntry sse = getSampleGoogleSpreadsheet();		
@@ -64,7 +60,7 @@ public class GoogleSpreadsheetUtilsTests {
 		Assert.assertNotNull(row);
 	}
 	
-	@Test
+	//@Test
 	public void shouldGetMJCell() throws IOException, ServiceException {
 		SpreadsheetEntry sse = getSampleGoogleSpreadsheet();
 		WorksheetEntry wse = GoogleSpreadsheetUtils.getWorksheet(gss
@@ -101,7 +97,7 @@ public class GoogleSpreadsheetUtilsTests {
 				mjCell.getParentRow().getMjCell(cellColIndex - 1).getParentRow().getRowIndex());		
 	}		
 
-	@Test
+	//@Test
 	public void shouldGetMJRow() throws IOException, ServiceException {
 		SpreadsheetEntry sse = getSampleGoogleSpreadsheet();
 		WorksheetEntry wse = GoogleSpreadsheetUtils.getWorksheet(gss
@@ -119,13 +115,12 @@ public class GoogleSpreadsheetUtilsTests {
 		Assert.assertNotNull(mjRow.getMjCells());
 		Assert.assertTrue(mjRow.getMjCells().size()>0);
 		
-		for (String tag : mjRow.getRowEntry().getCustomElements().getTags()) {
+/*		for (String tag : mjRow.getRowEntry().getCustomElements().getTags()) {
 		      //out.print(entry.getCustomElements().getValue(tag)+"\t");
 		      System.out.print(mjRow.getRowEntry().getCustomElements().getValue(tag)+" \t");
-		    }    
-		    
-		    System.out.println("");
-		    
+		}    		    
+		System.out.println("");
+*/		    
 		/*//row/list's rowIndex will be 1 less than cells rowIndex!
 		Assert.assertEquals(rowIndex - 1, mjCell.getParentRow().getRowIndex());
 		Assert.assertEquals(4, mjCell.getParentRow().getMjCells().size());
@@ -142,6 +137,124 @@ public class GoogleSpreadsheetUtilsTests {
 				mjCell.getParentRow().getMjCell(colIndex - 1).getParentRow().getRowIndex());		*/
 	}		
 
+	@Test
+	public void shouldBatchUpdateCells() throws IOException, ServiceException {
+		SpreadsheetEntry sse = getSampleGoogleSpreadsheet();
+		WorksheetEntry wse = GoogleSpreadsheetUtils.getWorksheet(gss
+				.getService(), sse, 0);
+		Assert.assertNotNull(wse);
+		
+		MJCellEntry mjCell_1 = GoogleSpreadsheetUtils.getMJCell(gss.getService(),
+				wse, 2, 1);
+		mjCell_1.getCellEntry().changeInputValueLocal("GSL-A21");
+		BatchUtils.setBatchId(mjCell_1.getCellEntry(), "1");
+		BatchUtils.setBatchOperationType(mjCell_1.getCellEntry(), BatchOperationType.UPDATE);
+		gss.addEntryToUpdate(mjCell_1);
+		
+		MJCellEntry mjCell_2 = GoogleSpreadsheetUtils.getMJCell(gss.getService(),
+				wse, 3, 1);
+		mjCell_2.getCellEntry().changeInputValueLocal("GSL-A21");
+		BatchUtils.setBatchId(mjCell_2.getCellEntry(), "2");
+		BatchUtils.setBatchOperationType(mjCell_2.getCellEntry(), BatchOperationType.UPDATE);
+		gss.addEntryToUpdate(mjCell_2);
+		
+		MJCellEntry mjCell_3 = GoogleSpreadsheetUtils.getMJCell(gss.getService(),
+				wse, 4, 1);
+		mjCell_3.getCellEntry().changeInputValueLocal("GSL-A21");
+		BatchUtils.setBatchId(mjCell_3.getCellEntry(), "3");
+		BatchUtils.setBatchOperationType(mjCell_3.getCellEntry(), BatchOperationType.UPDATE);
+		gss.addEntryToUpdate(mjCell_3);
+
+		MJCellEntry mjCell_4 = GoogleSpreadsheetUtils.getMJCell(gss.getService(),
+				wse, 5, 1);
+		mjCell_4.getCellEntry().changeInputValueLocal("GSL-A21");
+		BatchUtils.setBatchId(mjCell_4.getCellEntry(), "3");
+		BatchUtils.setBatchOperationType(mjCell_4.getCellEntry(), BatchOperationType.UPDATE);
+		gss.addEntryToUpdate(mjCell_4);
+		
+		GoogleSpreadsheetUtils.flush(gss.getService(), wse, gss.getBatchFeed());
+		
+	}		
+	
+	@Test //TODO: Need to review the code for dirty checking, avoid adding duplicate cell entry   
+	public void shouldBatchUpdateRows() throws IOException, ServiceException {
+		SpreadsheetEntry sse = getSampleGoogleSpreadsheet();
+		WorksheetEntry wse = GoogleSpreadsheetUtils.getWorksheet(gss
+				.getService(), sse, 0);
+		Assert.assertNotNull(wse);
+		
+		MJListEntry mjRow_1 = GoogleSpreadsheetUtils.getMJRow(gss.getService(),
+				wse, 1);
+
+		mjRow_1.getMjCell(1).getCellEntry().changeInputValueLocal("GSL-A219");
+		mjRow_1.getMjCell(1).setDirty();
+		
+		BatchUtils.setBatchId(mjRow_1.getMjCell(1).getCellEntry(), mjRow_1.getMjCell(1).getCellEntry().getId());
+		BatchUtils.setBatchOperationType(mjRow_1.getMjCell(1).getCellEntry(),
+				BatchOperationType.UPDATE);
+		
+		mjRow_1.setDirty();
+		gss.addEntryToUpdate(mjRow_1);
+	
+		MJListEntry mjRow_2 = GoogleSpreadsheetUtils.getMJRow(gss.getService(),
+				wse, 2);
+		mjRow_2.getMjCell(1).getCellEntry().changeInputValueLocal("GSL-A218");
+		mjRow_2.getMjCell(1).setDirty();
+		
+		BatchUtils.setBatchId(mjRow_2.getMjCell(1).getCellEntry(), mjRow_2.getMjCell(1).getCellEntry().getId());
+		BatchUtils.setBatchOperationType(mjRow_2.getMjCell(1).getCellEntry(),
+				BatchOperationType.UPDATE);
+		gss.addEntryToUpdate(mjRow_2);
+
+
+		MJListEntry mjRow_3 = GoogleSpreadsheetUtils.getMJRow(gss.getService(),
+				wse, 3);
+		mjRow_3.getMjCell(1).getCellEntry().changeInputValueLocal("GSL-A217");
+		mjRow_3.getMjCell(1).setDirty();
+		
+		BatchUtils.setBatchId(mjRow_3.getMjCell(1).getCellEntry(), mjRow_3.getMjCell(1).getCellEntry().getId());
+		BatchUtils.setBatchOperationType(mjRow_3.getMjCell(1).getCellEntry(),
+				BatchOperationType.UPDATE);
+		
+		gss.addEntryToUpdate(mjRow_3);
+
+		MJListEntry mjRow_4 = GoogleSpreadsheetUtils.getMJRow(gss.getService(),
+				wse, 4);
+		mjRow_4.getMjCell(1).getCellEntry().changeInputValueLocal("GSL-A216");
+		mjRow_4.getMjCell(1).setDirty();
+		
+		BatchUtils.setBatchId(mjRow_4.getMjCell(1).getCellEntry(), mjRow_4.getMjCell(1).getCellEntry().getId());
+		BatchUtils.setBatchOperationType(mjRow_4.getMjCell(1).getCellEntry(),
+				BatchOperationType.UPDATE);
+		gss.addEntryToUpdate(mjRow_4);
+		
+		GoogleSpreadsheetUtils.flush(gss.getService(), wse, gss.getBatchFeed());
+	}		
+
+	
+	@Test //TODO: need to resolve phantom row issue
+	public void shouldBatchDeleteRow() throws IOException, ServiceException {
+		SpreadsheetEntry sse = getSampleGoogleSpreadsheet();
+		WorksheetEntry wse = GoogleSpreadsheetUtils.getWorksheet(gss
+				.getService(), sse, 0);
+		Assert.assertNotNull(wse);
+		
+		MJListEntry mjRow_4 = GoogleSpreadsheetUtils.getMJRow(gss.getService(),
+				wse, 4);
+		for(MJCellEntry mjCell: mjRow_4.getMjCells()) {
+			mjCell.getCellEntry().changeInputValueLocal("");
+			mjCell.setDirty();
+		
+			BatchUtils.setBatchId(mjCell.getCellEntry(), mjCell.getCellEntry().getId());
+			BatchUtils.setBatchOperationType(mjCell.getCellEntry(),
+					BatchOperationType.UPDATE);
+		}
+		
+		mjRow_4.setDirty();
+		gss.addEntryToUpdate(mjRow_4);
+	
+		GoogleSpreadsheetUtils.flush(gss.getService(), wse, gss.getBatchFeed());
+	}		
 	
 	
 	private SpreadsheetEntry getSampleGoogleSpreadsheet() {
