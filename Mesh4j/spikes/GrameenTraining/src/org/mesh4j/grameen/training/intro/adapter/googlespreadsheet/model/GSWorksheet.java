a1524e8dc0c774d1b4d1053d7563c3a16fe3ae27
auni.ku@gmail.com <auni.ku@gmail.com@8593f39d-8f4b-0410-bdb6-4f53e344ee92>
2009-03-30 13:04:20 +0000

    added method for crud operation

--- Mesh4j/spikes/GrameenTraining/src/org/mesh4j/grameen/training/intro/adapter/googlespreadsheet/model/GSWorksheet.java
+++ Mesh4j/spikes/GrameenTraining/src/org/mesh4j/grameen/training/intro/adapter/googlespreadsheet/model/GSWorksheet.java
@@ -1,6 +1,9 @@
 package org.mesh4j.grameen.training.intro.adapter.googlespreadsheet.model;
 
 import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Map;
 
 import com.google.gdata.data.spreadsheet.WorksheetEntry;
@@ -9,7 +12,7 @@ public class GSWorksheet implements IGSElement{
 
 	// MODEL VARIABLES
 	private WorksheetEntry worksheet;  //represents the worksheet entry provided by google api
-	private Map<String, GSListEntry> rowList = new LinkedHashMap<String, GSListEntry>(); //represents the map of rows this worksheet contains
+	private Map<String, GSRow> rowList = new LinkedHashMap<String, GSRow>(); //represents the map of rows this worksheet contains
 	private int sheetIndex; //represents the order index of this worksheet in the container spreadsheet 
 	private boolean dirty = false; //flag represents worksheet content changed
 	private boolean deleteCandidate = false; //flag represents this worksheet is going to be deleted in next flush operation
@@ -20,7 +23,7 @@ public class GSWorksheet implements IGSElement{
 		super();
 		this.worksheet = worksheet;
 		this.sheetIndex = sheetIndex;
-		this.rowList = new LinkedHashMap<String, GSListEntry>();
+		this.rowList = new LinkedHashMap<String, GSRow>();
 	}
 	
 	public int getSheetIndex() {
@@ -30,7 +33,7 @@ public class GSWorksheet implements IGSElement{
 	public WorksheetEntry getWorksheet() {
 		return worksheet;
 	}
-	public Map<String, GSListEntry> getRowList() {
+	public Map<String, GSRow> getRowList() {
 		return rowList;
 	}
 
@@ -43,18 +46,55 @@ public class GSWorksheet implements IGSElement{
 		return null;
 	}
 
-	public void setDeleteCandiddate(boolean isDeleteCandidate) {
-		this.deleteCandidate = isDeleteCandidate;
+	public void setDeleteCandidate() {
+		this.deleteCandidate = true;
 	}
 	
 	public boolean isDeleteCandiddate() {
 		return this.deleteCandidate;
 	}
-	public void add(GSListEntry listEntry){
+	
+	/*
+	 * code blocked by sharif, 30-03-09. 
+	 * 
+	 * please use the method addChildEntry(IGSElement element) instead
+	 * also make sure ((GSRow)element).getRowIndex() returns its proper position
+	 * 
+	 * public void add(GSRow listEntry){
 		this.getRowList().put(String.valueOf(getRowList().size() +1), listEntry);
-	}
+	}*/
+	
 	public boolean isDirty() {
 		return this.dirty;
 	}	
+
+	public void setDirty() {
+		this.dirty = true;
+	}	
 	
+	public List<IGSElement> getChilds() {
+		Collection values = rowList.values();
+		List childs = new ArrayList(values);			
+		return childs;  
+	}
+	
+	public void addChildEntry(IGSElement element) {
+		element.setDirty();
+		String key = Integer.toString(((GSRow)element).getRowIndex());
+		this.rowList.put(key, (GSRow)element); 
+	}
+
+	public void deleteChildEntry(String key) {
+		this.rowList.get(key).setDirty();
+		this.rowList.get(key).setDeleteCandidate();
+	}
+
+	public IGSElement getChildEntry(String key) {
+		return this.rowList.get(key);
+	}
+
+	public void updateChildEntry(String key, IGSElement element) {
+		element.setDirty();
+		this.rowList.put(key, (GSRow)element);
+	}	
 }
