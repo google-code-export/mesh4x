3723bfd1f9cf3dee30bd0db9c0b058be2d92099c
auni.ku@gmail.com <auni.ku@gmail.com@8593f39d-8f4b-0410-bdb6-4f53e344ee92>
2009-04-02 12:08:54 +0000

    abstract class added which contains common functionality of subclasses

---
+++ Mesh4j/spikes/GrameenTraining/src/org/mesh4j/grameen/training/intro/adapter/googlespreadsheet/model/GSBaseElement.java
@@ -0,0 +1,95 @@
+package org.mesh4j.grameen.training.intro.adapter.googlespreadsheet.model;
+
+import java.util.Map;
+
+import com.google.gdata.data.BaseEntry;
+import com.google.gdata.data.IEntry;
+
+/**
+ * Provides an abstract class for various elements;
+ * Subclass should provide Child Element type C
+ * 
+ * @author sharif
+ * version 1.0, 30-03-09
+ *
+ */
+public abstract class GSBaseElement<C> implements IGSElement<C>{
+	
+	protected boolean dirty = false; //flag represents element content changed
+	protected boolean deleteCandidate = false; //flag represents this element is going to be deleted in next flush operation	
+	protected BaseEntry<?> baseEntry; 
+	
+	@SuppressWarnings("unchecked")
+	protected GSBaseElement parentElement;
+	protected Map<String, C> childElements;
+	
+	protected int elementListIndex;	
+
+	//Note:Google Spreadsheet's data row starts from it's 2nd row actually, 1st row contains the column headers. 
+	
+	public void setDeleteCandidate() {
+		this.deleteCandidate = true;
+	}
+	
+	public boolean isDeleteCandiddate() {
+		return this.deleteCandidate;
+	}
+	
+	public void setDirty() {
+		if( getParentElement() !=null )
+			((IGSElement<?>) parentElement).setDirty();
+		this.dirty = true;
+	}
+
+	public boolean isDirty() {
+		return this.dirty;
+	}	
+	
+	/**
+	 * return the unique element id specified for that element in the spreadsheet 
+	 *   
+	 * @return
+	 */
+	public String getId(){
+		return ((IEntry) this.baseEntry).getId();
+	}	
+	
+	public int getElementListIndex(){
+		return this.elementListIndex;
+	}	
+	
+	public BaseEntry<?> getBaseEntry(){
+		return baseEntry;
+	}
+
+	@SuppressWarnings("unchecked")
+	public GSBaseElement getParentElement(){
+		return parentElement;
+	}
+
+	public Map<String, C> getChildElements() {
+		return this.childElements;
+	}
+
+	/**
+	 * Note: this will set the element dirty 
+	 */	
+	public void addChildElement(String key, C element) {
+		((IGSElement<?>) element).setDirty();
+		this.childElements.put(key, element);
+	}
+
+	public void deleteChildElement(String key) {
+		((IGSElement<?>)this.childElements.get(key)).setDirty();
+		((IGSElement<?>)this.childElements.get(key)).setDeleteCandidate();
+	}
+
+	public C getChildElement(String key) {
+		return this.childElements.get(key);
+	}
+
+	public void updateChildElement(String key, C element) {
+		((IGSElement<?>)element).setDirty();
+		this.childElements.put(key, element);
+	}	
+}
