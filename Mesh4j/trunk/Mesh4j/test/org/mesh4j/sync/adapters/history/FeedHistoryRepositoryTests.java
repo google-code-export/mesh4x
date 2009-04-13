b9f7246bb989f918ae7812ab327314ce63545d52
jtondato@gmail.com <jtondato@gmail.com@8593f39d-8f4b-0410-bdb6-4f53e344ee92>
2009-04-13 11:56:58 +0000

    Add History change feature

--- Mesh4j/trunk/Mesh4j/test/org/mesh4j/sync/adapters/history/FeedHistoryRepositoryTests.java
+++ Mesh4j/trunk/Mesh4j/test/org/mesh4j/sync/adapters/history/FeedHistoryRepositoryTests.java
@@ -3,6 +3,8 @@ package org.mesh4j.sync.adapters.history;
 import java.util.Date;
 import java.util.List;
 
+import junit.framework.Assert;
+
 import org.junit.Test;
 import org.mesh4j.sync.adapters.feed.rss.RssSyndicationFormat;
 import org.mesh4j.sync.id.generator.IdGenerator;
@@ -19,10 +21,23 @@ public class FeedHistoryRepositoryTests {
 		
 		String syncId = IdGenerator.INSTANCE.newID();
 		
+		List<HistoryChange> changes = repo.getHistories(syncId);
+		Assert.assertNotNull(changes);
+		
+		int size = changes.size();
+		
 		HistoryChange historyChange = new HistoryChange(syncId, new History("jmt", new Date(), 1), "<foo>bar</foo>", HistoryType.ADD);
 		
 		repo.addHistoryChange(historyChange);
 		
-		List<HistoryChange> changes = repo.getHistories(syncId);
+		changes = repo.getHistories(syncId);
+		Assert.assertNotNull(changes);
+		Assert.assertEquals(size +1, changes.size());
+
+		HistoryChange lastChange = changes.get(size);
+		Assert.assertEquals(historyChange.getSyncId(), lastChange.getSyncId());
+		Assert.assertEquals(historyChange.getHistoryType(),  lastChange.getHistoryType());
+		Assert.assertEquals(historyChange.getSyncHistory(), lastChange.getSyncHistory());
+		Assert.assertEquals(historyChange.getPayload(), lastChange.getPayload());
 	}
 }
