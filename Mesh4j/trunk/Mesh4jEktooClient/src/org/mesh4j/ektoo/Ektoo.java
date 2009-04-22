2176e167b1d8176c54ebae0836374d5f853935ab
bhuiyan.mohammad.iklash@gmail.com <bhuiyan.mohammad.iklash@gmail.com@8593f39d-8f4b-0410-bdb6-4f53e344ee92>
2009-04-22 05:11:54 +0000

    Native LookAndFeel added.

--- Mesh4j/trunk/Mesh4jEktooClient/src/org/mesh4j/ektoo/Ektoo.java
+++ Mesh4j/trunk/Mesh4jEktooClient/src/org/mesh4j/ektoo/Ektoo.java
@@ -1,6 +1,8 @@
 package org.mesh4j.ektoo;
 
 import javax.swing.JFrame;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
 
 import org.mesh4j.ektoo.controller.EktooUIController;
 import org.mesh4j.ektoo.properties.PropertiesProvider;
@@ -21,6 +23,35 @@ public class Ektoo
 
 	public static void main(String[] args) 
 	{
-		Ektoo main = new Ektoo(); 
+    initLookAndFeel();
+    Ektoo main = new Ektoo(); 
 	}
+	
+	// TODO (NBL) make it configurable from properties file
+	private static void initLookAndFeel()
+	{
+    try
+    { 
+      //newly incorporated look & feel in J2SE 6.0
+      //UIManager.setLookAndFeel("com.sun.java.swing.plaf.nimbus.NimbusLookAndFeel");
+      //native look and feel
+      UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+    }
+    catch (ClassNotFoundException e)
+    { 
+      e.printStackTrace();
+    }
+    catch (InstantiationException e)
+    { 
+      e.printStackTrace();
+    }
+    catch (IllegalAccessException e)
+    { 
+      e.printStackTrace();
+    }
+    catch (UnsupportedLookAndFeelException e)
+    { 
+      e.printStackTrace();
+    }
+  }	
 }
