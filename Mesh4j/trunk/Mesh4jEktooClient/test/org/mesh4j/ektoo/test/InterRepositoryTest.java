7b7068dcce1cf17f1a7f5c525ede8d6a089d75ee
saiful.raju <saiful.raju@8593f39d-8f4b-0410-bdb6-4f53e344ee92>
2009-05-05 12:02:30 +0000

    added functionality of sync of access to excel (partial commit)

--- Mesh4j/trunk/Mesh4jEktooClient/test/org/mesh4j/ektoo/test/InterRepositoryTest.java
+++ Mesh4j/trunk/Mesh4jEktooClient/test/org/mesh4j/ektoo/test/InterRepositoryTest.java
@@ -1,5 +1,6 @@
 package org.mesh4j.ektoo.test;
 
+import java.io.IOException;
 import java.util.List;
 
 import junit.framework.Assert;
@@ -11,6 +12,7 @@ import org.mesh4j.ektoo.properties.PropertiesProvider;
 import org.mesh4j.sync.ISyncAdapter;
 import org.mesh4j.sync.SyncEngine;
 import org.mesh4j.sync.adapters.hibernate.HibernateContentAdapter;
+import org.mesh4j.sync.adapters.msaccess.MsAccessRDFSchemaGenerator;
 import org.mesh4j.sync.adapters.split.SplitAdapter;
 import org.mesh4j.sync.model.Item;
 import org.mesh4j.sync.payload.schema.ISchema;
@@ -20,7 +22,7 @@ import org.mesh4j.sync.test.utils.TestHelper;
 public class InterRepositoryTest {
 	
 	@Test
-	public void ShouldSyncMySQLToExcel(){
+	public void ShouldSyncMySQLToExcelByRDF(){
 		String user = "root";
 		String password = "test1234";
 		String tableName = "user";
@@ -28,17 +30,33 @@ public class InterRepositoryTest {
 		ISyncAdapterBuilder builder = new SyncAdapterBuilder(new PropertiesProvider());
 		ISyncAdapter sourceAsMySql =  builder.createMySQLAdapter(user, password,"localhost" ,3306,"mesh4xdb",tableName);
 		
-		
-		SplitAdapter splitAdapter = (SplitAdapter)sourceAsMySql;
-		
-		ISchema sourceSchema = ((HibernateContentAdapter)splitAdapter.getContentAdapter()).getMapping().getSchema();
-		
+		SplitAdapter splitAdapterSource = (SplitAdapter)sourceAsMySql;
+		ISchema sourceSchema = ((HibernateContentAdapter)splitAdapterSource.getContentAdapter()).getMapping().getSchema();
 		
 		ISyncAdapter targetAsExcel = builder.createMsExcelAdapter(TestHelper.baseDirectoryForTest() + "contentFile.xls", "user", "id", (IRDFSchema)sourceSchema);
-	
-		SyncEngine engine = new SyncEngine(splitAdapter,targetAsExcel);
+		
+		SyncEngine engine = new SyncEngine(splitAdapterSource,targetAsExcel);
 		List<Item> listOfConflicts = engine.synchronize();
 		Assert.assertEquals(0, listOfConflicts.size());
 		
 	}
+	
+	@Test
+	public void ShouldSyncMsAccessToExcelByRDF() throws IOException{
+		
+		String rdfBaseURl = "http://localhost:8080/mesh4x/feeds" +"/aktoo"+"#";
+		ISyncAdapterBuilder builder = new SyncAdapterBuilder(new PropertiesProvider());
+		
+		ISyncAdapter sourceAsAccess = builder.createMsAccessAdapter(TestHelper.baseDirectoryForTest() +"aktoo.mdb" , "aktoo");
+		SplitAdapter splitAdapterSource = (SplitAdapter)sourceAsAccess;
+		IRDFSchema sourceSchema = MsAccessRDFSchemaGenerator.extractRDFSchema(TestHelper.baseDirectoryForTest() +"aktoo.mdb", "aktoo", "aktoo", rdfBaseURl);
+		
+		ISyncAdapter targetAsExcel = builder.createMsExcelAdapter(TestHelper.baseDirectoryForTest() + "contentFile.xls", "aktoo", "id", sourceSchema);
+		
+		SyncEngine engine = new SyncEngine(splitAdapterSource,targetAsExcel);
+		List<Item> listOfConflicts = engine.synchronize();
+		Assert.assertEquals(0, listOfConflicts.size());
+		
+	}
+	
 }
