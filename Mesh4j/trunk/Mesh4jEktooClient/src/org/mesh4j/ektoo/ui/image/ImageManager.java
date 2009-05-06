2cff1fcea00e1e137e0081e3a134620dc75a89e7
bhuiyan.mohammad.iklash@gmail.com <bhuiyan.mohammad.iklash@gmail.com@8593f39d-8f4b-0410-bdb6-4f53e344ee92>
2009-05-06 04:56:11 +0000

    Various Graphics Resource Manager

---
+++ Mesh4j/trunk/Mesh4jEktooClient/src/org/mesh4j/ektoo/ui/image/ImageManager.java
@@ -0,0 +1,108 @@
+package org.mesh4j.ektoo.ui.image;
+
+import java.awt.Image;
+
+import javax.swing.Icon;
+
+import org.mesh4j.ektoo.ui.EktooUI;
+
+import com.swtdesigner.SwingResourceManager;
+
+public class ImageManager 
+{
+	
+  public static Image getLogoSmall() 
+  {
+    return SwingResourceManager.getImage(EktooUI.class, "/instedd_small.jpg");
+  }
+
+  public static Icon getUndefinedSourceIcon() 
+  {
+    return SwingResourceManager.getIcon(EktooUI.class, "/undefinedDataSource.png");
+  }
+  
+  public static Icon getDataSourceSmall() 
+  {
+    return SwingResourceManager.getIcon(EktooUI.class, "/dataSourceSmall.png");
+  }
+  
+  public static Icon getDatabaseConnectionIcon() 
+  {
+    return SwingResourceManager.getIcon(EktooUI.class, "/connect_db.jpg");
+  }
+
+  public static Icon getSyncModeIcon(boolean sendChanges, boolean receiveChanges)
+  {
+    if(sendChanges && receiveChanges)
+    {
+      return SwingResourceManager.getIcon(EktooUI.class, "/2WaySync.png");
+    }
+    else if(sendChanges)
+    {
+      return SwingResourceManager.getIcon(EktooUI.class, "/sendChangesOnly.png");
+    }
+    else 
+    {
+      return SwingResourceManager.getIcon(EktooUI.class, "/receiveChangesOnly.png");
+    }
+  }  
+  
+  public static Icon getSourceImage(String source, boolean remote)
+  {
+    System.out.println("Source?" + source);
+    if(source == null)
+    {
+      return SwingResourceManager.getIcon(EktooUI.class, "/undefinedDataSource.png");
+    } 
+    else if (source.startsWith("Cloud"))
+    {
+      return SwingResourceManager.getIcon(EktooUI.class, "/httpDataSource.png");      
+    } 
+    else if (source.startsWith("KML"))
+    {
+      return SwingResourceManager.getIcon(EktooUI.class, "/kmlDataSource.png");
+    } 
+    else if (source.matches("Access"))
+    {
+      return SwingResourceManager.getIcon(EktooUI.class, "/msAccessDataSource.png");
+    } 
+    else if (source.matches("Excel"))
+    {
+      return SwingResourceManager.getIcon(EktooUI.class, "/msExcelDataSource.png");
+    } 
+    else 
+    {
+      return SwingResourceManager.getIcon(EktooUI.class, "/undefinedDataSource.png");
+    }
+  }
+  
+  public static Icon getStatusErrorIcon() 
+  {
+    return SwingResourceManager.getIcon(EktooUI.class, "/error.png");
+  }
+
+  public static Icon getStatusInProgressIcon() 
+  {
+    return SwingResourceManager.getIcon(EktooUI.class, "/inProgress.gif");
+  }
+  
+  public static Icon getStatusOkIcon() {
+    return SwingResourceManager.getIcon(EktooUI.class, "/ok.png");
+  }
+  
+  public static Icon getStatusProcessingIcon() {
+    return SwingResourceManager.getIcon(EktooUI.class, "/processing.png");
+  }
+ 
+  public static Icon getTrademarkIcon() {
+    return SwingResourceManager.getIcon(EktooUI.class, "/mesh4x.png");
+  }  
+  
+  public static Icon getCloudErrorIcon() {
+    return SwingResourceManager.getIcon(EktooUI.class, "/cloudError.jpg");
+  }
+
+  public static Icon getCloudIcon() {
+    return SwingResourceManager.getIcon(EktooUI.class, "/cloud.jpg");
+  }
+}
