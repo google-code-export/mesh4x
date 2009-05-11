d2348fc96f6d060a2c429fa9f268043608d11340
auni.ku@gmail.com <auni.ku@gmail.com@8593f39d-8f4b-0410-bdb6-4f53e344ee92>
2009-05-11 05:53:23 +0000

    Added docservice to upload a document in the google doc

--- Mesh4j/spikes/GrameenTraining/src/org/mesh4j/grameen/training/intro/adapter/googlespreadsheet/GoogleSpreadsheet.java
+++ Mesh4j/spikes/GrameenTraining/src/org/mesh4j/grameen/training/intro/adapter/googlespreadsheet/GoogleSpreadsheet.java
@@ -5,30 +5,29 @@ import org.mesh4j.grameen.training.intro.adapter.googlespreadsheet.model.GSWorks
 import org.mesh4j.sync.validations.Guard;
 import org.mesh4j.sync.validations.MeshException;
 
+import com.google.gdata.client.docs.DocsService;
 import com.google.gdata.client.spreadsheet.FeedURLFactory;
 import com.google.gdata.client.spreadsheet.SpreadsheetService;
-import com.google.gdata.util.AuthenticationException;
 
-
-
-public class GoogleSpreadsheet implements IGoogleSpreadSheet{
+public class GoogleSpreadsheet implements IGoogleSpreadSheet {
 
 	// MODEL VARIABLES
 	private String spreadsheetFileId;
 	private String username;
 	private String password;
 	private GSSpreadsheet<GSWorksheet> spreadsheet;
-	
-	private SpreadsheetService service;
+
+	private SpreadsheetService spreadsheetService;
+	DocsService docService;
 	private FeedURLFactory factory;
-	
+
 	private boolean dirty = false;
-			
+
 	public static final String VISIBILITY_PRIVATE = "private";
 	public static final String VISIBILITY_PUBLIC = "public";
-	
+
 	public static final String PROJECTION_PUBLIC = "full";
-	public static final String PROJECTION_VALUES = "values";	
+	public static final String PROJECTION_VALUES = "values";
 	public static final String PROJECTION_BASIC = "basic";
 
 	public static final String DOC_ROOT = "http://spreadsheets.google.com/feeds";
@@ -37,28 +36,18 @@ public class GoogleSpreadsheet implements IGoogleSpreadSheet{
 	public static final String DOC_ELEMENT_TYPE_ROW = "list";
 	public static final String DOC_ELEMENT_TYPE_WORKSHEET = "worksheets";
 	public static final String DOC_ELEMENT_TYPE_SPREADSHEET = "spreadsheets";
-	
-		
+
 	// BUSINESS METHODS
-	
+
 	/**
-	 * initialize service and feedURL factory 
+	 * initialize service and feedURL factory
 	 */
 	private void init() {
-		
-		this.service = new SpreadsheetService("Mesh4j");
-		this.service.setProtocolVersion(SpreadsheetService.Versions.V1);
-
-		try {
-			this.service.setUserCredentials(this.username, this.password);
-
-		} catch (AuthenticationException e) {
-			throw new MeshException(e);
-		}
-
+		this.spreadsheetService = GoogleSpreadsheetUtils.getSpreadsheetService(username, password);
+		this.docService = GoogleSpreadsheetUtils.getDocService(username, password);
 		this.factory = FeedURLFactory.getDefault();
-	}	
-	
+	}
+
 	/**
 	 * load spreadsheet by spreadsheet file ID
 	 * 
@@ -66,6 +55,7 @@ public class GoogleSpreadsheet implements IGoogleSpreadSheet{
 	 * @param username
 	 * @param password
 	 */
+	@SuppressWarnings("unchecked")
 	public GoogleSpreadsheet(String spreadsheetFileId, String username,
 			String password) {
 		super();
@@ -77,17 +67,17 @@ public class GoogleSpreadsheet implements IGoogleSpreadSheet{
 		this.spreadsheetFileId = spreadsheetFileId;
 		this.username = username;
 		this.password = password;
-		
+
 		init();
-		
+
 		try {
 			this.spreadsheet = GoogleSpreadsheetUtils.getGSSpreadsheet(
-					this.factory, this.service, spreadsheetFileId);
+					this.factory, this.spreadsheetService, spreadsheetFileId);
 		} catch (Exception e) {
 			throw new MeshException(e);
-		}	
-	}	
-	
+		}
+	}
+
 	/**
 	 * load spreadsheet by sheet index
 	 * 
@@ -95,88 +85,95 @@ public class GoogleSpreadsheet implements IGoogleSpreadSheet{
 	 * @param username
 	 * @param password
 	 */
-	public GoogleSpreadsheet(int sheetIndex, String username,
-			String password) {
+	@SuppressWarnings("unchecked")
+	public GoogleSpreadsheet(int sheetIndex, String username, String password) {
 		super();
 		Guard.argumentNotNullOrEmptyString(username, "username");
 		Guard.argumentNotNullOrEmptyString(password, "password");
 
 		init();
-		
+
 		try {
 			this.spreadsheet = GoogleSpreadsheetUtils.getGSSpreadsheet(
-					this.factory, this.service, sheetIndex);
+					this.factory, this.spreadsheetService, sheetIndex);
 		} catch (Exception e) {
 			throw new MeshException(e);
 		}
 	}
-	
-	public SpreadsheetService getService() {
-		return service;
+
+	public SpreadsheetService getSpreadsheetService() {
+		return spreadsheetService;
+	}
+
+	public DocsService getDocService() {
+		return docService;
 	}
 
 	public FeedURLFactory getFactory() {
 		return factory;
 	}
 
+	@SuppressWarnings("unchecked")
 	public GSSpreadsheet getGSSpreadsheet() {
 		return this.spreadsheet;
 	}
 
 	/**
 	 * get specific worksheet by sheet name/title
+	 * 
 	 * @param sheetName
 	 * @return
 	 */
+	@SuppressWarnings("unchecked")
 	public GSWorksheet getGSWorksheet(String sheetName) {
 		return this.spreadsheet.getGSWorksheetBySheetName(sheetName);
-	}	
+	}
 
 	/**
 	 * get specific worksheet by sheetIndex
+	 * 
 	 * @param sheetIndex
 	 * @return
 	 */
+	@SuppressWarnings("unchecked")
 	public GSWorksheet getGSWorksheet(int sheetIndex) {
 		return this.spreadsheet.getGSWorksheet(sheetIndex);
-	}	
-	
+	}
+
 	public void setDirty() {
-		this.dirty = true;		
+		this.dirty = true;
 	}
 
-/*	public void addEntryToUpdate(GSCell toUpdate){
-		batchFeed.getEntries().add(toUpdate.getCellEntry());
-	}
-
-	public void addEntryToUpdate(GSRow toUpdate){		
-		for(IGSElement gsCell : toUpdate.getGsCells()){
-			if(gsCell.isDirty()){
-				addEntryToUpdate((GSCell)gsCell);
-				toUpdate.setDirty();
-			}	
-		}
-	}
-*/	
+	/*
+	 * public void addEntryToUpdate(GSCell toUpdate){
+	 * batchFeed.getEntries().add(toUpdate.getCellEntry()); }
+	 * 
+	 * public void addEntryToUpdate(GSRow toUpdate){ for(IGSElement gsCell :
+	 * toUpdate.getGsCells()){ if(gsCell.isDirty()){
+	 * addEntryToUpdate((GSCell)gsCell); toUpdate.setDirty(); } } }
+	 */
 	/**
-	 * transfer the current state of the spreadsheet in memory to spreadsheet file in weeb
+	 * transfer the current state of the spreadsheet in memory to spreadsheet
+	 * file in web
 	 */
 	public void flush() {
-		if(this.dirty)
-			GoogleSpreadsheetUtils.flush(this.service, this.spreadsheet);
+		if (this.dirty)
+			GoogleSpreadsheetUtils.flush(this.spreadsheetService,
+					this.spreadsheet);
 	}
-		
+
 	/**
-	 * reload spreadsheet file to memory 
+	 * reload spreadsheet file to memory
 	 */
-	public void refresh(){
+	@SuppressWarnings("unchecked")
+	public void refresh() {
 		try {
 			this.spreadsheet = GoogleSpreadsheetUtils.getGSSpreadsheet(
-					this.factory, this.service, spreadsheetFileId);
-			this.dirty = false;		
+					this.factory, this.spreadsheetService, spreadsheetFileId);
+			this.dirty = false;
 		} catch (Exception e) {
 			throw new MeshException(e);
 		}
 	}
-	
+
 }
