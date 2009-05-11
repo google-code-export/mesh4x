f88860db95d715e2a267d19feb53dc845c61a225
jtondato@gmail.com <jtondato@gmail.com@8593f39d-8f4b-0410-bdb6-4f53e344ee92>
2009-05-11 16:28:37 +0000

    Add FeedUI for rss20 and atom 10 data source options.

---
+++ Mesh4j/trunk/Mesh4jEktooClient/src/org/mesh4j/ektoo/ui/FeedUI.java
@@ -0,0 +1,147 @@
+package org.mesh4j.ektoo.ui;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Point;
+import java.awt.Rectangle;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.beans.PropertyChangeEvent;
+import java.io.File;
+import java.io.IOException;
+
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.filechooser.FileNameExtensionFilter;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.mesh4j.ektoo.controller.FeedUIController;
+import org.mesh4j.ektoo.ui.translator.EktooUITranslator;
+
+public class FeedUI extends AbstractUI {
+
+
+	private static final long serialVersionUID = 2457237653577593698L;
+
+	private static final Log LOGGER = LogFactory.getLog(FeedUI.class);
+	
+	// MODEL VARIABLES
+	private JLabel labelFileName = null;
+	private JTextField txtFileName = null;
+	private JButton btnFile = null;
+	
+	private FeedUIController controller;
+	private JFileChooser fileChooser = null;
+	private File file = null;
+
+	// BUSINESS METHODS
+	public FeedUI(String fileName, FeedUIController controller) {
+		super();
+		this.controller = controller;
+		this.controller.addView(this);
+		this.initialize();
+		this.file = new File(fileName);
+		this.txtFileName.setText(this.file.getName());
+	}
+
+	private void initialize() 
+	{
+		this.setLayout(null);
+		this.setBackground(Color.WHITE);
+		this.add(getFileNameLabel(), null);
+		this.add(getFileNameText(), null);
+		this.add(getBtnFile(), null);
+	}
+
+	private JLabel getFileNameLabel() {
+		if (labelFileName == null) {
+			labelFileName = new JLabel();
+			labelFileName.setText(EktooUITranslator.getFeedFileNameLabel());
+			labelFileName.setSize(new Dimension(85, 16));
+			labelFileName.setPreferredSize(new Dimension(85, 16));
+			labelFileName.setLocation(new Point(8, 9));
+		}
+		return labelFileName;
+	}
+
+	private JTextField getFileNameText() {
+		if (txtFileName == null) {
+			txtFileName = new JTextField();
+			txtFileName.setBounds(new Rectangle(99, 8, 149, 20));
+		}
+		return txtFileName;
+	}
+
+	public JButton getBtnFile() {
+		if (btnFile == null) {
+			btnFile = new JButton();
+			btnFile.setText(EktooUITranslator.getBrowseButtonLabel());
+			btnFile.setBounds(new Rectangle(259, 8, 34, 20));
+			btnFile.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					int returnVal = getFileChooser().showOpenDialog(btnFile);
+					if (returnVal == JFileChooser.APPROVE_OPTION) {
+						// System.out.println("You chose to open this file: " +
+						// getFileChooser().getSelectedFile().getName());
+						setFile(getFileChooser().getSelectedFile());
+						if (getFile() != null) {
+							txtFileName.setText(getFile().getName());
+						}
+					}
+				}
+			});
+		}
+		return btnFile;
+	}
+
+	
+	public String getFileName() {
+		try {
+			return this.file.getCanonicalPath();
+		} catch (IOException e) {
+            LOGGER.debug(e.getMessage());
+			// nothing to do
+			return null;
+		}
+	}
+	
+	public FeedUIController getController() {
+		return controller;
+	}
+	
+	public void setFileChooser(JFileChooser fileChooser) {
+		this.fileChooser = fileChooser;
+	}
+
+	public JFileChooser getFileChooser() {
+		if (fileChooser == null){
+			fileChooser = new JFileChooser();
+			fileChooser.setAcceptAllFileFilterUsed(false);
+			fileChooser.setFileFilter(new FileNameExtensionFilter(EktooUITranslator.getXMLFileSelectorTitle(), "xml", "XML"));
+		}		
+		return fileChooser;
+	}
+	
+	public void setFile(File file) {
+		this.file = file;
+	}
+
+	public File getFile() {
+		return file;
+	}
+
+  @Override
+  public void modelPropertyChange(final PropertyChangeEvent evt)
+  {
+    if ( evt.getPropertyName().equals( FeedUIController.FILE_NAME_PROPERTY))
+    {
+      String newStringValue = evt.getNewValue().toString();
+      if (!  getFileNameText().getText().equals(newStringValue))
+        getFileNameText().setText(newStringValue);
+    }
+  }
+
+}
