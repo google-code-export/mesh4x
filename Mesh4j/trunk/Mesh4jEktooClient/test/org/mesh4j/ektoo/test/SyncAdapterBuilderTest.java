2912827722b82be88e091213c33e49faf5832038
auni.ku@gmail.com <auni.ku@gmail.com@8593f39d-8f4b-0410-bdb6-4f53e344ee92>
2009-05-20 07:57:31 +0000

    test case added for google spreadsheet

--- Mesh4j/trunk/Mesh4jEktooClient/test/org/mesh4j/ektoo/test/SyncAdapterBuilderTest.java
+++ Mesh4j/trunk/Mesh4jEktooClient/test/org/mesh4j/ektoo/test/SyncAdapterBuilderTest.java
@@ -13,24 +13,39 @@ import org.mesh4j.ektoo.GoogleSpreadSheetInfo;
 import org.mesh4j.ektoo.ISyncAdapterBuilder;
 import org.mesh4j.ektoo.SyncAdapterBuilder;
 import org.mesh4j.ektoo.properties.PropertiesProvider;
+import org.mesh4j.grameen.training.intro.adapter.googlespreadsheet.GoogleSpreadSheetContentAdapter;
 import org.mesh4j.grameen.training.intro.adapter.googlespreadsheet.GoogleSpreadsheet;
+import org.mesh4j.grameen.training.intro.adapter.googlespreadsheet.GoogleSpreadsheetUtils;
 import org.mesh4j.grameen.training.intro.adapter.googlespreadsheet.IGoogleSpreadSheet;
 import org.mesh4j.grameen.training.intro.adapter.googlespreadsheet.mapping.GoogleSpreadsheetToRDFMapping;
+import org.mesh4j.grameen.training.intro.adapter.googlespreadsheet.mapping.IGoogleSpreadsheetToXMLMapping;
+import org.mesh4j.grameen.training.intro.adapter.googlespreadsheet.model.GSCell;
+import org.mesh4j.grameen.training.intro.adapter.googlespreadsheet.model.GSRow;
+import org.mesh4j.grameen.training.intro.adapter.googlespreadsheet.model.GSWorksheet;
+
+
 import org.mesh4j.sync.ISyncAdapter;
 import org.mesh4j.sync.adapters.feed.rss.RssSyndicationFormat;
 import org.mesh4j.sync.adapters.hibernate.EntityContent;
+import org.mesh4j.sync.adapters.split.SplitAdapter;
 import org.mesh4j.sync.id.generator.IdGenerator;
 import org.mesh4j.sync.model.IContent;
 import org.mesh4j.sync.model.Item;
 import org.mesh4j.sync.model.Sync;
 import org.mesh4j.sync.payload.schema.ISchema;
 import org.mesh4j.sync.payload.schema.rdf.IRDFSchema;
+
+import org.mesh4j.sync.payload.schema.rdf.RDFInstance;
+import org.mesh4j.sync.payload.schema.rdf.RDFSchema;
+import org.mesh4j.sync.test.utils.TestHelper;
+
 import org.mesh4j.sync.utils.XMLHelper;
+import org.mesh4j.sync.validations.MeshException;
+
+import com.google.gdata.client.docs.DocsService;
 
 public class SyncAdapterBuilderTest {
 
-	
-	
 	@Test
 	public void ShouldCreateFolderSyncAdapter() throws IOException{
 		
@@ -38,34 +53,311 @@ public class SyncAdapterBuilderTest {
 		ISyncAdapter folderAdapter = adapterBuilder.createFolderAdapter(TestHelper.baseDirectoryForTest() + "sourcefolder");
 		Assert.assertNotNull(folderAdapter);
 		Assert.assertNotNull(folderAdapter.getAll());
-	}
+	}	
 	
-	/**
-	 * This test assumes you have a worksheet named "user_source" in your spreadsheet
-	 * with the provided gmail account(gspreadsheet.test@gmail.com)
-	 */
-	@Test
-	public void shouldCreateGoogleSpreadSheetAdapter(){
-		
+	@Test(expected=MeshException.class)
+	public void shouldNotCreatePlainXMLBasedGoogleSpreadSheetAdapterWhenSpreadsheetWithThegivenNameNotAvailable(){
 		GoogleSpreadSheetInfo spreadSheetInfo = new GoogleSpreadSheetInfo(
-				"peo4fu7AitTo8e3v0D8FCew",
+				"pLUqch-enpf1-GcqnD6qjSA",
 				"gspreadsheet.test@gmail.com",
 				"java123456",
 				"id",
 				"user_source",
 				"user"
 				);
-		
-		String rdfUrl = "http://localhost:8080/mesh4x/feeds";
-		
-		IGoogleSpreadSheet gss = new GoogleSpreadsheet(spreadSheetInfo.getGoogleSpreadSheetId(), spreadSheetInfo.getUserName(), spreadSheetInfo.getPassWord());
-		IRDFSchema rdfSchema = GoogleSpreadsheetToRDFMapping.extractRDFSchema(gss, spreadSheetInfo.getSheetName(), rdfUrl);
-			
+	
 		ISyncAdapterBuilder adapterBuilder = new SyncAdapterBuilder(new PropertiesProvider());
-		ISyncAdapter syncAdapterA = adapterBuilder.createGoogleSpreadSheetAdapter(spreadSheetInfo, rdfSchema);
+		ISyncAdapter syncAdapterA = adapterBuilder.createPlainXMLBasedGoogleSpreadSheetAdapter(spreadSheetInfo);
+		Assert.assertNull(syncAdapterA);
+	}
+
+	@Test
+	public void shouldCreatePlainXMLBasedGoogleSpreadSheetAdapterWhenSpreadsheetWithTheGivenNameAvailable() throws Exception{
+		GoogleSpreadSheetInfo spreadSheetInfo = new GoogleSpreadSheetInfo(
+				"spreadsheettest2",
+				"gspreadsheet.test@gmail.com",
+				"java123456",
+				"id",
+				"user_source",
+				"user"
+				);
+
+		ISyncAdapterBuilder adapterBuilder = new SyncAdapterBuilder(new PropertiesProvider());
+		ISyncAdapter syncAdapter = adapterBuilder.createPlainXMLBasedGoogleSpreadSheetAdapter(spreadSheetInfo);
+		Assert.assertNotNull(syncAdapter);
+	}
+	
+	@Test //ok
+	public void shouldCreateRdfBasedGoogleSpreadSheetAdapterUsingOwnRDFSchemaIfDataRowAvailable(){
+			GoogleSpreadSheetInfo spreadSheetInfo = new GoogleSpreadSheetInfo(
+					"spreadsheettest2",
+					"gspreadsheet.test@gmail.com",
+					"java123456",
+					"id",
+					"user",
+					"user"
+					);
+			
+			//prepare spreadsheet for test (make sure at least one data row available in the sheet)
+			IGoogleSpreadSheet spreadSheet = new GoogleSpreadsheet(spreadSheetInfo.getGoogleSpreadSheetName(), 
+					spreadSheetInfo.getUserName(), spreadSheetInfo.getPassWord());
+			
+			Assert.assertNotNull(spreadSheet.getGSSpreadsheet());
+			
+			GSWorksheet<GSRow<GSCell>> worksheet = spreadSheet.getGSWorksheet(spreadSheetInfo.getSheetName());
+			if( worksheet!= null )
+				spreadSheet.getGSSpreadsheet().deleteChildElement(String.valueOf(worksheet.getElementListIndex()));
+
+			worksheet = spreadSheet.getGSWorksheet(spreadSheetInfo.getSheetName()+"_sync");
+			if( worksheet!= null )
+				spreadSheet.getGSSpreadsheet().deleteChildElement(String.valueOf(worksheet.getElementListIndex()));
+		
+			GSWorksheet<GSRow<GSCell>> newWorksheet = GoogleSpreadsheetUtils.getOrCreateWorkSheetIfAbsent(spreadSheet.getGSSpreadsheet(), spreadSheetInfo.getSheetName());
+			
+			//create the header row
+			GSRow<GSCell> headerRow = GoogleSpreadsheetUtils.getOrCreateHeaderRowIfAbsent(newWorksheet);
+			GoogleSpreadsheetUtils.getOrCreateHeaderCellIfAbsent(headerRow, "id");
+			GoogleSpreadsheetUtils.getOrCreateHeaderCellIfAbsent(headerRow, "name");
+			GoogleSpreadsheetUtils.getOrCreateHeaderCellIfAbsent(headerRow, "pass");
+		
+			GSRow<GSCell> dataRow = newWorksheet.createNewRow(2);
+			dataRow.createNewCell(headerRow.getChildElement("id").getColIndex(), "id", "gsl-a123");
+			dataRow.createNewCell(headerRow.getChildElement("name").getColIndex(), "name", "sharif");
+			dataRow.createNewCell(headerRow.getChildElement("pass").getColIndex(), "pass", "mesh4x");				
+
+			spreadSheet.setDirty();
+			spreadSheet.flush();			
+			
+			ISyncAdapterBuilder adapterBuilder = new SyncAdapterBuilder(new PropertiesProvider());
+			ISyncAdapter syncAdapter = adapterBuilder.createRdfBasedGoogleSpreadSheetAdapter(spreadSheetInfo, null);		
+			
+			Assert.assertNotNull(syncAdapter);
+			Assert.assertEquals(1, syncAdapter.getAll().size());
+			
+			IRDFSchema rdfSchema = (IRDFSchema)((GoogleSpreadSheetContentAdapter)((SplitAdapter)syncAdapter).getContentAdapter()).getSchema();
+			Assert.assertNotNull(rdfSchema);
+			Assert.assertEquals(3, rdfSchema.getPropertyCount());
+	}	
+
+	@Test(expected=MeshException.class) //ok
+	public void shouldNotCreateRdfBasedGoogleSpreadSheetAdapterUsingOwnRDFSchemaIfNoDataRowAvailable(){
+			GoogleSpreadSheetInfo spreadSheetInfo = new GoogleSpreadSheetInfo(
+					"spreadsheettest2",
+					"gspreadsheet.test@gmail.com",
+					"java123456",
+					"id",
+					"user",
+					"user"
+					);
+			//prepare spreadsheet for test (remove all data rows from spreadsheet)
+			IGoogleSpreadSheet spreadSheet = new GoogleSpreadsheet(spreadSheetInfo.getGoogleSpreadSheetName(), 
+					spreadSheetInfo.getUserName(), spreadSheetInfo.getPassWord());
+			
+			GSWorksheet worksheet = GoogleSpreadsheetUtils.getOrCreateWorkSheetIfAbsent(spreadSheet.getGSSpreadsheet(), spreadSheetInfo.getSheetName());
+			//clean up data row if any
+			int rowCount = worksheet.getChildElements().size();
+			
+			if(rowCount > 1){
+				//remove all the data row 
+				for (int i = 2; i <= rowCount; i++){
+					worksheet.deleteChildElement(String.valueOf(i));
+				}
+			}
+			
+			spreadSheet.setDirty();
+			spreadSheet.flush();		
+		
+			//create the adapter now
+			ISyncAdapterBuilder adapterBuilder = new SyncAdapterBuilder(new PropertiesProvider());
+			ISyncAdapter syncAdapter = adapterBuilder.createRdfBasedGoogleSpreadSheetAdapter(spreadSheetInfo, null);		
+			Assert.assertNull(syncAdapter);
+	}	
+	
+	@Test //ok
+	public void shouldCreateRdfBasedGoogleSpreadSheetAdapterUsingOtherRDFWhenSpreadsheetAvailableAndWorksheetAvailableWithCompatibleSchema(){
+		GoogleSpreadSheetInfo spreadSheetInfo = new GoogleSpreadSheetInfo(
+				"spreadsheettest2",
+				"gspreadsheet.test@gmail.com",
+				"java123456",
+				"id",
+				"user",
+				"user"
+				);
+		
+		//prepare spreadsheet for test (make sure at least one data row available in the sheet)
+		IGoogleSpreadSheet spreadSheet = new GoogleSpreadsheet(spreadSheetInfo.getGoogleSpreadSheetName(), 
+				spreadSheetInfo.getUserName(), spreadSheetInfo.getPassWord());
+		
+		Assert.assertNotNull(spreadSheet.getGSSpreadsheet());
+		
+		GSWorksheet<GSRow<GSCell>> worksheet = spreadSheet.getGSWorksheet(spreadSheetInfo.getSheetName());
+		if( worksheet!= null )
+			spreadSheet.getGSSpreadsheet().deleteChildElement(String.valueOf(worksheet.getElementListIndex()));
+
+		worksheet = spreadSheet.getGSWorksheet(spreadSheetInfo.getSheetName()+"_sync");
+		if( worksheet!= null )
+			spreadSheet.getGSSpreadsheet().deleteChildElement(String.valueOf(worksheet.getElementListIndex()));
+	
+		GSWorksheet<GSRow<GSCell>> newWorksheet = GoogleSpreadsheetUtils.getOrCreateWorkSheetIfAbsent(spreadSheet.getGSSpreadsheet(), spreadSheetInfo.getSheetName());
+		
+		//create the header row
+		GSRow<GSCell> headerRow = GoogleSpreadsheetUtils.getOrCreateHeaderRowIfAbsent(newWorksheet);
+		GoogleSpreadsheetUtils.getOrCreateHeaderCellIfAbsent(headerRow, "id");
+		GoogleSpreadsheetUtils.getOrCreateHeaderCellIfAbsent(headerRow, "name");
+		GoogleSpreadsheetUtils.getOrCreateHeaderCellIfAbsent(headerRow, "pass");
+	
+		GSRow<GSCell> dataRow = newWorksheet.createNewRow(2);
+		dataRow.createNewCell(headerRow.getChildElement("id").getColIndex(), "id", "gsl-a123");
+		dataRow.createNewCell(headerRow.getChildElement("name").getColIndex(), "name", "sharif");
+		dataRow.createNewCell(headerRow.getChildElement("pass").getColIndex(), "pass", "mesh4x");	
+		
+		spreadSheet.setDirty();
+		spreadSheet.flush();		
+		
+		ISyncAdapterBuilder adapterBuilder = new SyncAdapterBuilder(new PropertiesProvider());
+		
+		RDFSchema sourceSchema = new RDFSchema("user",
+				"http://localhost:8080/mesh4x/feeds/user#", "user");
+		sourceSchema.addStringProperty("id", "id", "en");
+		sourceSchema.addStringProperty("name", "Name", "en");
+		sourceSchema.addStringProperty("pass", "pass", "en");
+
+		ISyncAdapter syncAdapter = adapterBuilder.createRdfBasedGoogleSpreadSheetAdapter(spreadSheetInfo, (IRDFSchema) sourceSchema);
+		Assert.assertNotNull(syncAdapter);
+		
+		IRDFSchema rdfSchema = (IRDFSchema)((GoogleSpreadSheetContentAdapter)
+				((SplitAdapter)syncAdapter).getContentAdapter()).getSchema();
+		Assert.assertNotNull(rdfSchema);
+		Assert.assertTrue(sourceSchema.isCompatible(rdfSchema));		
+	}		
+
+	
+	@Test(expected=MeshException.class) //ok
+	public void shouldNotCreateRdfBasedGoogleSpreadSheetAdapterUsingOtherRDFWhenSpreadsheetAvailableAndWorksheetAvailableWithNonCompatibleSchema(){
+		GoogleSpreadSheetInfo spreadSheetInfo = new GoogleSpreadSheetInfo(
+				"spreadsheettest2",
+				"gspreadsheet.test@gmail.com",
+				"java123456",
+				"id",
+				"user",
+				"user"
+				);
+		
+		//prepare spreadsheet for test (make sure at least one data row available in the sheet)
+		IGoogleSpreadSheet spreadSheet = new GoogleSpreadsheet(spreadSheetInfo.getGoogleSpreadSheetName(), 
+				spreadSheetInfo.getUserName(), spreadSheetInfo.getPassWord());
+		
+		Assert.assertNotNull(spreadSheet.getGSSpreadsheet());
+		
+		GSWorksheet<GSRow<GSCell>> worksheet = spreadSheet.getGSWorksheet(spreadSheetInfo.getSheetName());
+		
+		if(worksheet != null)
+			spreadSheet.getGSSpreadsheet().deleteChildElement(String.valueOf(worksheet.getElementListIndex()));
+		
+		GSWorksheet<GSRow<GSCell>> newWorksheet = GoogleSpreadsheetUtils.getOrCreateWorkSheetIfAbsent(spreadSheet.getGSSpreadsheet(), spreadSheetInfo.getSheetName());
+		
+		//create/change the header row
+		GSRow<GSCell> headerRow = GoogleSpreadsheetUtils.getOrCreateHeaderRowIfAbsent(newWorksheet);
+		GoogleSpreadsheetUtils.getOrCreateHeaderCellIfAbsent(headerRow, "id");
+		GoogleSpreadsheetUtils.getOrCreateHeaderCellIfAbsent(headerRow, "name");
+		GoogleSpreadsheetUtils.getOrCreateHeaderCellIfAbsent(headerRow, "age");
+		GoogleSpreadsheetUtils.getOrCreateHeaderCellIfAbsent(headerRow, "phone");
+	
+		//add data
+		GSRow<GSCell> dataRow = newWorksheet.createNewRow(newWorksheet.getChildElements().size()+1);
+		dataRow.createNewCell(headerRow.getChildElement("id").getColIndex(), "id", "gsl-a123");
+		dataRow.createNewCell(headerRow.getChildElement("name").getColIndex(), "name", "sharif");
+		dataRow.createNewCell(headerRow.getChildElement("age").getColIndex(), "age", "13");
+		dataRow.createNewCell(headerRow.getChildElement("phone").getColIndex(), "phone", "12345");	
+			
+		spreadSheet.setDirty();
+		spreadSheet.flush();		
+		
+		ISyncAdapterBuilder adapterBuilder = new SyncAdapterBuilder(new PropertiesProvider());
+		
+		RDFSchema sourceSchema = new RDFSchema("user",
+				"http://localhost:8080/mesh4x/feeds/user#", "user");
+		sourceSchema.addStringProperty("id", "id", "en");
+		sourceSchema.addStringProperty("name", "Name", "en");
+		sourceSchema.addStringProperty("pass", "pass", "en");
+
+		ISyncAdapter syncAdapter = adapterBuilder.createRdfBasedGoogleSpreadSheetAdapter(spreadSheetInfo, (IRDFSchema) sourceSchema);
+		Assert.assertNull(syncAdapter);
+		
+		IRDFSchema rdfSchema = (IRDFSchema)((GoogleSpreadSheetContentAdapter)((SplitAdapter)syncAdapter).getContentAdapter()).getSchema();
+		Assert.assertNotNull(rdfSchema);
+		Assert.assertFalse(sourceSchema.isCompatible(rdfSchema));		
+	}	
+	
+	@Test //ok
+	public void shouldCreateRdfBasedGoogleSpreadSheetAdapterUsingOtherRDFWhenSpreadsheetAvailableButWorksheetNotAvailable(){
+		GoogleSpreadSheetInfo spreadSheetInfo = new GoogleSpreadSheetInfo(
+				"spreadsheettest2",
+				"gspreadsheet.test@gmail.com",
+				"java123456",
+				"id",
+				"user",
+				"user"
+				);
+		
+		//prepare spreadsheet for test (make no worksheet available with the name contained in sheetName variable)
+		IGoogleSpreadSheet spreadSheet = new GoogleSpreadsheet(spreadSheetInfo.getGoogleSpreadSheetName(), 
+				spreadSheetInfo.getUserName(), spreadSheetInfo.getPassWord());
+		
+		Assert.assertNotNull(spreadSheet.getGSSpreadsheet());
+		
+		GSWorksheet<GSRow<GSCell>> worksheet = spreadSheet.getGSWorksheet(spreadSheetInfo.getSheetName());
+		if( worksheet!= null )
+			spreadSheet.getGSSpreadsheet().deleteChildElement(String.valueOf(worksheet.getElementListIndex()));
+
+		worksheet = spreadSheet.getGSWorksheet(spreadSheetInfo.getSheetName()+"_sync");
+		if( worksheet!= null )
+			spreadSheet.getGSSpreadsheet().deleteChildElement(String.valueOf(worksheet.getElementListIndex()));
+		
+		spreadSheet.setDirty();
+		spreadSheet.flush();		
+		
+		ISyncAdapterBuilder adapterBuilder = new SyncAdapterBuilder(new PropertiesProvider());
+		
+		RDFSchema sourceSchema = new RDFSchema("user", "http://localhost:8080/mesh4x/feeds/user#", "user");
+		sourceSchema.addStringProperty("id", "id", "en");
+		sourceSchema.addStringProperty("name", "Name", "en");
+		sourceSchema.addStringProperty("pass", "pass", "en");
+
+		ISyncAdapter syncAdapter = adapterBuilder.createRdfBasedGoogleSpreadSheetAdapter(spreadSheetInfo, (IRDFSchema) sourceSchema);
+		Assert.assertNotNull(syncAdapter);
+		
+		IRDFSchema rdfSchema = (IRDFSchema)((GoogleSpreadSheetContentAdapter)((SplitAdapter)syncAdapter).getContentAdapter()).getSchema();
+		Assert.assertNotNull(rdfSchema);
+		Assert.assertTrue(sourceSchema.isCompatible(rdfSchema));
+
+		ISyncAdapter syncAdapterA = adapterBuilder.createRdfBasedGoogleSpreadSheetAdapter(spreadSheetInfo, rdfSchema);
 		Assert.assertNotNull(syncAdapterA);
 		
+		Assert.assertEquals(0, syncAdapter.getAll().size());
+		syncAdapter.add(makeRDFItem(sourceSchema));
+		Assert.assertEquals(1, syncAdapter.getAll().size());
 	}
+	
+	
+	@Test
+	public void shouldCreateRdfBasedGoogleSpreadSheetAdapterUsingOtherRDFWhenSpreadsheetNotAvailable(){		
+		IGoogleSpreadsheetToXMLMapping mapping = createSampleGoogleSpreadsheetToRDFMapping();
+		
+		GoogleSpreadSheetInfo spreadSheetInfo = new GoogleSpreadSheetInfo(
+				"no spreadsheet",
+				"gspreadsheet.test@gmail.com",
+				"java123456",
+				mapping.getIdColumnName(),
+				mapping.getSheetName(),
+				mapping.getType()
+				);
+			
+		ISyncAdapterBuilder adapterBuilder = new SyncAdapterBuilder(new PropertiesProvider());
+		ISyncAdapter syncAdapter = adapterBuilder.createRdfBasedGoogleSpreadSheetAdapter(spreadSheetInfo, mapping.getSchema());
+		Assert.assertNotNull(syncAdapter);
+	}	
+	
 	
 	@Test
 	public void shouldCreateMsAccessAdapter() throws Exception{
@@ -145,38 +437,66 @@ public class SyncAdapterBuilderTest {
 		adapterBuilder.createMsExcelAdapter(null, "user", "id");
 	}
 	
-	
+	private static final String SHEET_NAME = "user";
+	private static final String COLUMN_PASS = "Pass";
+	private static final String COLUMN_NAME = "Name";
+	private static final String COLUMN_ID = "Id";
+		
+	private GoogleSpreadsheetToRDFMapping createSampleGoogleSpreadsheetToRDFMapping() {
+
+		String sheetName = SHEET_NAME;
+
+		RDFSchema schema = new RDFSchema(sheetName, "http://localhost:8080/mesh4x/feeds/user#", sheetName);
+		schema.addStringProperty("id", "id", "en");
+		schema.addStringProperty("name", "Name", "en");
+		schema.addStringProperty("pass", "pass", "en");
+
+		RDFInstance rdfInstance = schema.createNewInstance("uri:urn:id");
+
+		long millis = System.currentTimeMillis();
+		String name = "Name: " + millis;
+		String code = "GSL-A219";
+		String pass = "mesh4j";
+
+		rdfInstance.setProperty(COLUMN_ID, code);
+		rdfInstance.setProperty(COLUMN_NAME, name);
+		rdfInstance.setProperty(COLUMN_PASS, pass);
+
+		return new GoogleSpreadsheetToRDFMapping(schema, SHEET_NAME, "id", null, new DocsService(""));
+	}		
 	
 	private Item getItem() throws DocumentException {
 		
 		String id = IdGenerator.INSTANCE.newID();
-		String rawDataAsXML = "<user>" +
+/*		String rawDataAsXML = "<user>" +
 								"<id>"+id+"</id>" +
 								"<age>25</age>" +
 								"<name>Marcelo</name>" +
 								"<city>Buens aires</city>" +
 								"<country>Argentina</country>" +
-								"</user>";
-		
+								"</user>";*/
+		String rawDataAsXML = "<user>" +
+				"<id>"+id+"</id>" +
+				"<name>Marcelo</name>" +
+				"<pass>Buensaires</pass>" +
+				"</user>";
+				
 		Element payload = XMLHelper.parseElement(rawDataAsXML);
 		IContent content = new EntityContent(payload, "user", id);
 		Sync sync = new Sync(IdGenerator.INSTANCE.newID(), "Raju", new Date(), false);
 		return new Item(content, sync);
 	}
-	
-	protected Item makeRDFItem(IRDFSchema schema){
-		
-		String id = IdGenerator.INSTANCE.newID();
-		
-		String rawDataAsXML = "<ektoo>" +
-		"<ID>"+id+"</ID>" +
-		"<Name>Raju</Name>" +
-		"<Age>25</Age>" +
-		"</ektoo>";
+
+	private Item makeRDFItem(IRDFSchema schema){		
+		String id = IdGenerator.INSTANCE.newID();		
+		String rawDataAsXML = "<user>" +
+				"<id>"+id+"</id>" +
+				"<name>Raju</name>" +
+				"<pass>25</pass>" +
+				"</user>";
 
 		Element payload = XMLHelper.parseElement(rawDataAsXML);
 		payload = schema.getInstanceFromPlainXML(id, payload, ISchema.EMPTY_FORMATS);
-		
 		IContent content = new EntityContent(payload, "ektoo", id);
 		Sync sync = new Sync(IdGenerator.INSTANCE.newID(), "Raju", new Date(), false);
 		return new Item(content, sync);
