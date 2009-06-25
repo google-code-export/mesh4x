e8be06063866164a9fd5e7b3b1163e4be54e5eb4
auni.ku@gmail.com <auni.ku@gmail.com@8593f39d-8f4b-0410-bdb6-4f53e344ee92>
2009-06-25 04:36:48 +0000

    reformated

--- Mesh4j/trunk/Mesh4j/etc/localProperties/mesh4j_table_mysql.sql
+++ Mesh4j/trunk/Mesh4j/etc/localProperties/mesh4j_table_mysql.sql
@@ -1,14 +1,12 @@
-  
 DROP TABLE mesh_example;
 
 CREATE TABLE mesh_example (
   uid varchar(100) PRIMARY KEY not null,
   name varchar(100) not null,
   pass varchar(100) not null);
-  
- insert into mesh_example (uid, name, pass) values (1, "jmt", "123")
-  
-  
+
+insert into mesh_example (uid, name, pass) values (1, "jmt", "123");
+
 DROP TABLE mesh_example_1;
 
 CREATE TABLE mesh_example_1 (
@@ -19,7 +17,5 @@ CREATE TABLE mesh_example_1 (
   vfloat float(5,3) not null  
 );
 
-
-insert into mesh_example_1 (uid, vString, vdecimal, vdouble, vfloat) values('2', 'jmt', 3.257, 3.257, 3.257)
-insert into mesh_example_1 (uid, vString, vdecimal, vdouble, vfloat) values('3', 'jmt2', 3, 3, 3)
-
+insert into mesh_example_1 (uid, vString, vdecimal, vdouble, vfloat) values('2', 'jmt', 3.257, 3.257, 3.257);
+insert into mesh_example_1 (uid, vString, vdecimal, vdouble, vfloat) values('3', 'jmt2', 3, 3, 3);
