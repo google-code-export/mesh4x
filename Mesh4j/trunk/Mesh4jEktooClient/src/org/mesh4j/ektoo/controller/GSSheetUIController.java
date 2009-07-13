f5bbc15568a12a0a4d9631af404ba7c195bf652c
auni.ku@gmail.com <auni.ku@gmail.com@8593f39d-8f4b-0410-bdb6-4f53e344ee92>
2009-07-13 12:53:06 +0000

    updated for multi key support

--- Mesh4j/trunk/Mesh4jEktooClient/src/org/mesh4j/ektoo/controller/GSSheetUIController.java
+++ Mesh4j/trunk/Mesh4jEktooClient/src/org/mesh4j/ektoo/controller/GSSheetUIController.java
@@ -13,7 +13,6 @@ import org.mesh4j.sync.adapters.composite.CompositeSyncAdapter;
 import org.mesh4j.sync.adapters.googlespreadsheet.GoogleSpreadSheetContentAdapter;
 import org.mesh4j.sync.adapters.googlespreadsheet.GoogleSpreadsheet;
 import org.mesh4j.sync.adapters.googlespreadsheet.IGoogleSpreadSheet;
-import org.mesh4j.sync.adapters.googlespreadsheet.mapping.GoogleSpreadsheetToRDFMapping;
 import org.mesh4j.sync.adapters.split.SplitAdapter;
 import org.mesh4j.sync.payload.schema.rdf.IRDFSchema;
 import org.mesh4j.sync.validations.Guard;
@@ -25,7 +24,7 @@ public class GSSheetUIController extends AbstractUIController {
 	public static final String USER_PASSWORD_PROPERTY = "UserPassword";
 	public static final String SPREADSHEET_NAME_PROPERTY = "SpreadsheetName";
 	public static final String WORKSHEET_NAME_PROPERTY = "WorksheetName";
-	public static final String UNIQUE_COLUMN_NAME_PROPERTY = "UniqueColumnName";
+	public static final String UNIQUE_COLUMN_NAME_PROPERTY = "UniqueColumnNames";
 	public static final String GOOGLE_SPREADSHEET_PROPERTY = "GSpreadsheet";
 
 	// MODEL VARIABLES
@@ -55,8 +54,8 @@ public class GSSheetUIController extends AbstractUIController {
 		setModelProperty(WORKSHEET_NAME_PROPERTY, worksheetName);
 	}
 
-	public void changeUniqueColumnName(String uniqueColumnName) {
-		setModelProperty(UNIQUE_COLUMN_NAME_PROPERTY, uniqueColumnName);
+	public void changeUniqueColumnNames(String[] uniqueColumnNames) {
+		setModelProperty(UNIQUE_COLUMN_NAME_PROPERTY, uniqueColumnNames);
 	}
 
 	public void changeGSpreadsheet(IGoogleSpreadSheet gSpreadsheet) {
@@ -74,23 +73,23 @@ public class GSSheetUIController extends AbstractUIController {
 			model.setGSpreadsheet((GoogleSpreadsheet) gss);
 		}
 		
-		ArrayList<String> pks = new ArrayList<String>();
-		pks.add(model.getUniqueColumnName());
+//		ArrayList<String> pks = new ArrayList<String>();
+//		pks.add(model.getUniqueColumnNames());
 		
-		IRDFSchema rdfSchema = GoogleSpreadsheetToRDFMapping.extractRDFSchema(
-			model.getGSpreadsheet(), 
-			model.getWorksheetName(),
-			pks, 
-			/*model.getLastUpdatedColumnName(),*/
-			this.adapterBuilder.getBaseRDFUrl());
+//		IRDFSchema rdfSchema = GoogleSpreadsheetToRDFMapping.extractRDFSchema(
+//			model.getGSpreadsheet(), 
+//			model.getWorksheetName(),
+//			pks, 
+//			/*model.getLastUpdatedColumnName(),*/
+//			this.adapterBuilder.getBaseRDFUrl());
 
 		GoogleSpreadSheetInfo spreadSheetInfo = new GoogleSpreadSheetInfo(
 				model.getSpreadsheetName(), model.getUserName(), 
-				model.getUserPassword(), model.getUniqueColumnName(), 
+				model.getUserPassword(), model.getUniqueColumnNames(), 
 				model.getWorksheetName(), model.getWorksheetName());
 		
 //		return adapterBuilder.createRdfBasedGoogleSpreadSheetAdapter(spreadSheetInfo, rdfSchema);
-		return adapterBuilder.createRdfBasedGoogleSpreadSheetAdapter(spreadSheetInfo, model.getGSpreadsheet(), rdfSchema);
+		return adapterBuilder.createRdfBasedGoogleSpreadSheetAdapter(spreadSheetInfo, model.getGSpreadsheet(), null);
 	}
 
 	@Override
@@ -115,22 +114,22 @@ public class GSSheetUIController extends AbstractUIController {
 		
 		
 		String worksheetName = model.getWorksheetName(); 
-		String uniqueColumnName = model.getUniqueColumnName();
+		String[] uniqueColumnNames = model.getUniqueColumnNames();
 
 		// when user provides a new spreadsheet name in target, worksheet name and
 		// uniqueColumn name is not taken from user as input rather it will be taken from source schema.
 		// if source schema is not available this is ended up with an exception
-		if( worksheetName == null && uniqueColumnName == null){
+		if( worksheetName == null && (uniqueColumnNames == null || uniqueColumnNames.length == 0)){
 			if(rdfSchema != null) {
 				worksheetName = rdfSchema.getName();
-				uniqueColumnName = rdfSchema.getIdentifiablePropertyNames().get(0);
+				uniqueColumnNames = (String[]) rdfSchema.getIdentifiablePropertyNames().toArray();
 			} else
 				throw new MeshException("Unable to create target datasource without source schema.");
 		}
 		
 		GoogleSpreadSheetInfo spreadSheetInfo = new GoogleSpreadSheetInfo(
 				model.getSpreadsheetName(), model.getUserName(), 
-				model.getUserPassword(), uniqueColumnName, worksheetName, worksheetName);
+				model.getUserPassword(), uniqueColumnNames, worksheetName, worksheetName);
 		
 		return adapterBuilder.createRdfBasedGoogleSpreadSheetAdapter(spreadSheetInfo, rdfSchema);
 	}
