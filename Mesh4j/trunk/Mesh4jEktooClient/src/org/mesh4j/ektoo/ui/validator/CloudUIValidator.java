ab2856c040dda9e15cc8fb3607be5e2a45e6933c
jtondato@gmail.com <jtondato@gmail.com@8593f39d-8f4b-0410-bdb6-4f53e344ee92>
2009-07-30 21:34:54 +0000

    Kml generation, lat/log from address field or from lat/log fields

--- Mesh4j/trunk/Mesh4jEktooClient/src/org/mesh4j/ektoo/ui/validator/CloudUIValidator.java
+++ Mesh4j/trunk/Mesh4jEktooClient/src/org/mesh4j/ektoo/ui/validator/CloudUIValidator.java
@@ -15,17 +15,17 @@ import org.mesh4j.ektoo.validator.AbstractValidator;
 public class CloudUIValidator extends AbstractValidator {
 	
 	// MODEL VARIABLES
-	private boolean mustValidateDataset = true;
+	private boolean mustValidateDatasetAndMeshGroup = true;
 	
 	// BUSINESS METHODS
 	public CloudUIValidator(JComponent form, AbstractModel model, List<JComponent> uiFieldListForValidation) {
 		super(form, model, uiFieldListForValidation);
-		this.mustValidateDataset = true;
+		this.mustValidateDatasetAndMeshGroup = true;
 	}
 	
 	public CloudUIValidator(JComponent form, AbstractModel model, List<JComponent> uiFieldListForValidation, boolean mustValidateDataset) {
 		super(form, model, uiFieldListForValidation);
-		this.mustValidateDataset = mustValidateDataset;
+		this.mustValidateDatasetAndMeshGroup = mustValidateDataset;
 	}
 	
 	@Override
@@ -52,27 +52,36 @@ public class CloudUIValidator extends AbstractValidator {
 			}
 		}
 		
+		String meshName = ui.getMeshText().getText();
 		if ((getUiFieldListForValidation().isEmpty() || getUiFieldListForValidation().contains(ui.getMeshText()))){
-				String meshName = ui.getMeshText().getText();
 				if(meshName.trim().length() == 0) {
-					setError(ui.getMeshText(), EktooUITranslator.getErrorEmptyOrNull(CloudUIController.MESH_NAME_PROPERTY));
-					isValid = false;
+					if(mustValidateDatasetAndMeshGroup){
+						setError(ui.getMeshText(), EktooUITranslator.getErrorEmptyOrNull(CloudUIController.MESH_NAME_PROPERTY));
+						isValid = false;
+					}
 				}else if(meshName.contains(" ") || meshName.contains("/") || meshName.contains("\\")){
 					setError(ui.getMeshText(), EktooUITranslator.getErrorInvalidMeshName());
 					isValid = false;
 				}
 		}
 
-		if(this.mustValidateDataset){
-			if ((getUiFieldListForValidation().isEmpty() || getUiFieldListForValidation().contains(ui.getDataSetText()))){
-				String dataSetName = ui.getDataSetText().getText();
-				if(dataSetName.trim().length() == 0) {
+		String dataSetName = ui.getDataSetText().getText();
+		if ((getUiFieldListForValidation().isEmpty() || getUiFieldListForValidation().contains(ui.getDataSetText()))){
+			if(dataSetName.trim().length() == 0) {
+				if(mustValidateDatasetAndMeshGroup){
 					setError(ui.getDataSetText(), EktooUITranslator.getErrorEmptyOrNull(CloudUIController.DATASET_NAME_PROPERTY));
 					isValid = false;
-				}else if(dataSetName.contains(" ") || dataSetName.contains("/") || dataSetName.contains("\\")){
-					setError(ui.getDataSetText(), EktooUITranslator.getErrorInvalidDataSetName());
-					isValid = false;
 				}
+			}else if(dataSetName.contains(" ") || dataSetName.contains("/") || dataSetName.contains("\\")){
+				setError(ui.getDataSetText(), EktooUITranslator.getErrorInvalidDataSetName());
+				isValid = false;
+			}
+		}
+		
+		if(isValid){
+			if(meshName.trim().length() == 0 && dataSetName.trim().length() > 0){
+				setError(ui.getMeshText(), EktooUITranslator.getErrorEmptyOrNull(CloudUIController.MESH_NAME_PROPERTY));
+				isValid = false;
 			}
 		}
 
