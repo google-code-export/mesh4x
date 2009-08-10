7bbbb65add13fde95f173b2c888b427a5f71f3ab
jtondato@gmail.com <jtondato@gmail.com@8593f39d-8f4b-0410-bdb6-4f53e344ee92>
2009-08-10 20:30:04 +0000

    Xform: Add tests for HttpSyncAdapter and SyncServer, getXform, upload meshgroup and dataset, get all data set in a meshgroup expressing the content as xform (filters, translations, etc). Simplify the Http sync adapter API for create URLs. Add to XmlHelper canonicalize xml protocol element and attributes sorting.

--- Mesh4j/trunk/Mesh4j/test/org/mesh4j/sync/payload/schema/xform/SchemaToXFormTranslatorTests.java
+++ Mesh4j/trunk/Mesh4j/test/org/mesh4j/sync/payload/schema/xform/SchemaToXFormTranslatorTests.java
@@ -1,9 +1,13 @@
 package org.mesh4j.sync.payload.schema.xform;
 
+import java.io.File;
 import java.io.IOException;
 
+import org.dom4j.DocumentHelper;
+import org.dom4j.Element;
 import org.junit.Assert;
 import org.junit.Test;
+import org.mesh4j.sync.payload.schema.Schema;
 import org.mesh4j.sync.payload.schema.rdf.IRDFSchema;
 import org.mesh4j.sync.payload.schema.rdf.RDFSchema;
 import org.mesh4j.sync.utils.FileUtils;
@@ -12,21 +16,38 @@ import org.mesh4j.sync.utils.XMLHelper;
 public class SchemaToXFormTranslatorTests {
 
 	@Test
-	public void shouldGenerateXForm() throws IOException{
-		
-		RDFSchema schema = new RDFSchema("example", "http://mesh4x/example#", "example");
-		schema.addStringProperty("string", "string", IRDFSchema.DEFAULT_LANGUAGE);
-		schema.addIntegerProperty("integer", "int", IRDFSchema.DEFAULT_LANGUAGE);
-		schema.addBooleanProperty("boolean", "boolean", IRDFSchema.DEFAULT_LANGUAGE);
-		schema.addDateTimeProperty("datetime", "datetime", IRDFSchema.DEFAULT_LANGUAGE);
-		schema.addDoubleProperty("double", "double", IRDFSchema.DEFAULT_LANGUAGE);
-		schema.addLongProperty("long", "long", IRDFSchema.DEFAULT_LANGUAGE);
-		schema.addDecimalProperty("decimal", "decimal", IRDFSchema.DEFAULT_LANGUAGE);
-		
-		String xml = XMLHelper.canonicalizeXML(SchemaToXFormTranslator.translate(schema));
+	public void shouldNotTranslateNullSchema(){
+		Assert.assertEquals("", SchemaToXFormTranslator.translate(null));
+	}
 
-		String xmlForm = XMLHelper.canonicalizeXML(new String(FileUtils.read(this.getClass().getResource("XFormSchemaForTests.txt").getFile())));
+	@Test
+	public void shouldNotTranslateNoRDFSchema(){
+		Assert.assertEquals("", SchemaToXFormTranslator.translate(new Schema(DocumentHelper.createElement("foo"))));
+	}
+	
+	@Test
+	public void shouldTranslateRDFSchema() throws IOException{
+		String xml = new String(FileUtils.read(new File(getClass().getResource("xform.txt").getFile())));
+		Element element = XMLHelper.parseElement(xml);
 		
-		Assert.assertEquals(xmlForm, xml);
+		String xmlXForm = SchemaToXFormTranslator.translate(getDefaultRDFSchema());
+		Element elementXForm = XMLHelper.parseElement(xmlXForm);
+		
+		Assert.assertEquals(XMLHelper.canonicalizeXML(element), XMLHelper.canonicalizeXML(elementXForm));
+		
+	}
+	
+	private IRDFSchema getDefaultRDFSchema() {
+		RDFSchema rdfSchema = new RDFSchema("Oswego", "http://mesh4x/Oswego#", "Oswego");
+		rdfSchema.addBooleanProperty("ill", "ill", IRDFSchema.DEFAULT_LANGUAGE);
+		rdfSchema.addDateTimeProperty("dateOnset", "dateOnSet", IRDFSchema.DEFAULT_LANGUAGE);
+		rdfSchema.addDecimalProperty("decimal", "decimal", IRDFSchema.DEFAULT_LANGUAGE);
+		rdfSchema.addDoubleProperty("AgeDouble", "ageDouble", IRDFSchema.DEFAULT_LANGUAGE);
+		rdfSchema.addIntegerProperty("AgeInt", "ageInt", IRDFSchema.DEFAULT_LANGUAGE);
+		rdfSchema.addLongProperty("AgeLong", "ageLong", IRDFSchema.DEFAULT_LANGUAGE);
+		rdfSchema.addStringProperty("name", "name", IRDFSchema.DEFAULT_LANGUAGE);
+	
+		return rdfSchema;
+	
 	}
 }
