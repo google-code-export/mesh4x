5e11b4d4ab4bf1f24d655732b19678a57218ffd2
auni.ku@gmail.com <auni.ku@gmail.com@8593f39d-8f4b-0410-bdb6-4f53e344ee92>
2009-09-09 04:27:47 +0000

    Resolved: Issue #125

--- Mesh4j/trunk/Mesh4j/src/org/mesh4j/sync/adapters/hibernate/mapping/HibernateToRDFMapping.java
+++ Mesh4j/trunk/Mesh4j/src/org/mesh4j/sync/adapters/hibernate/mapping/HibernateToRDFMapping.java
@@ -1,6 +1,7 @@
 package org.mesh4j.sync.adapters.hibernate.mapping;
 
 import java.io.Serializable;
+import java.math.BigDecimal;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -17,6 +18,9 @@ import org.mesh4j.sync.payload.schema.rdf.IRDFSchema;
 import org.mesh4j.sync.payload.schema.rdf.RDFInstance;
 import org.mesh4j.sync.utils.XMLHelper;
 import org.mesh4j.sync.validations.Guard;
+
+import com.hp.hpl.jena.datatypes.RDFDatatype;
+import com.hp.hpl.jena.datatypes.TypeMapper;
 
 public class HibernateToRDFMapping extends AbstractRDFIdentifiableMapping implements IHibernateToXMLMapping {
 	
@@ -117,7 +121,16 @@ public class HibernateToRDFMapping extends AbstractRDFIdentifiableMapping implem
 			return (DefaultElement)XMLHelper.parseElement(sb.toString());
 			
 		} else {
-			return meshId;
+			//Issue#125:Sharif:08/09/09
+			String idPropertyType = this.rdfSchema.getPropertyType(this.rdfSchema.getIdentifiablePropertyNames().get(0));
+			RDFDatatype dataType = TypeMapper.getInstance().getTypeByName(idPropertyType);
+			
+			if (IRDFSchema.XLS_LONG.equals(idPropertyType) || IRDFSchema.XLS_INTEGER.equals(idPropertyType)) {
+				return (Serializable) dataType.parse(meshId);
+			} else {
+				return meshId;
+			}	
+			//Issue#125
 		}
 	}
 }
