599274ad67065aeb046b35a5ddd6938cb20834c6
juan.wajnerman <juan.wajnerman@8593f39d-8f4b-0410-bdb6-4f53e344ee92>
2010-05-20 14:35:18 +0000

    Image resources moved to package

--- Mesh4j/trunk/Meshes/src/org/mesh4j/meshes/ui/component/MeshesTree.java
+++ Mesh4j/trunk/Meshes/src/org/mesh4j/meshes/ui/component/MeshesTree.java
@@ -1,10 +1,16 @@
 package org.mesh4j.meshes.ui.component;
 
+import java.io.IOException;
+import java.util.List;
+
 import javax.swing.JTree;
 import javax.swing.ToolTipManager;
 import javax.swing.tree.DefaultMutableTreeNode;
 import javax.swing.tree.DefaultTreeModel;
 import javax.swing.tree.TreeSelectionModel;
+
+import org.mesh4j.meshes.io.ConfigurationManager;
+import org.mesh4j.meshes.model.Mesh;
 
 public class MeshesTree extends JTree {
 
@@ -25,25 +31,35 @@ public class MeshesTree extends JTree {
 	}
 	
 	private void createNodes(DefaultMutableTreeNode top) {
-	    DefaultMutableTreeNode oswego = new DefaultMutableTreeNode("Oswego");
-	    DefaultMutableTreeNode mesh2 = new DefaultMutableTreeNode("Mesh2");
-	    
-	    DefaultMutableTreeNode tomato = new DefaultMutableTreeNode("TomatoOutbreakSurvey");
-	    
-	    tomato.add(new DefaultMutableTreeNode("Access"));
-	    tomato.add(new DefaultMutableTreeNode("Local Excel file"));
-	    tomato.add(new DefaultMutableTreeNode("Johns Google Spreadsheet"));
-	    tomato.add(new DefaultMutableTreeNode("Map"));
-	    
-	    DefaultMutableTreeNode clinics = new DefaultMutableTreeNode("Clinics and Hospitals in Oswego");
-	    
-	    clinics.add(new DefaultMutableTreeNode("Shared KML"));
-	    
-	    oswego.add(tomato);
-	    oswego.add(clinics);
-	    
-	    top.add(oswego);
-	    top.add(mesh2);
+//	    DefaultMutableTreeNode oswego = new DefaultMutableTreeNode("Oswego");
+//	    DefaultMutableTreeNode mesh2 = new DefaultMutableTreeNode("Mesh2");
+//	    
+//	    DefaultMutableTreeNode tomato = new DefaultMutableTreeNode("TomatoOutbreakSurvey");
+//	    
+//	    tomato.add(new DefaultMutableTreeNode("Access"));
+//	    tomato.add(new DefaultMutableTreeNode("Local Excel file"));
+//	    tomato.add(new DefaultMutableTreeNode("Johns Google Spreadsheet"));
+//	    tomato.add(new DefaultMutableTreeNode("Map"));
+//	    
+//	    DefaultMutableTreeNode clinics = new DefaultMutableTreeNode("Clinics and Hospitals in Oswego");
+//	    
+//	    clinics.add(new DefaultMutableTreeNode("Shared KML"));
+//	    
+//	    oswego.add(tomato);
+//	    oswego.add(clinics);
+//	    
+//	    top.add(oswego);
+//	    top.add(mesh2);
+		ConfigurationManager confMgr = new ConfigurationManager();
+		List<Mesh> meshes;
+		try {
+			meshes = confMgr.getAllMeshes();
+		} catch (IOException e) {
+			throw new Error(e);
+		}
+		for (Mesh mesh : meshes) {
+			top.add(new DefaultMutableTreeNode(mesh.getName()));
+		}
 	}
 
 }
--- Mesh4j/trunk/Meshes/src/org/mesh4j/meshes/ui/resource/ResourceManager.java
+++ Mesh4j/trunk/Meshes/src/org/mesh4j/meshes/ui/resource/ResourceManager.java
@@ -2,29 +2,32 @@ package org.mesh4j.meshes.ui.resource;
 
 import java.awt.Image;
 import java.awt.Toolkit;
+import java.net.URL;
 
 public class ResourceManager {
 	
 	private ResourceManager() {}
 	
-	public static Image getLogo() {
-		Image image = Toolkit.getDefaultToolkit().getImage("images/instedd_small.png");
+	private static Image getImage(String path) {
+		URL url = ResourceManager.class.getResource(path);
+		Image image = Toolkit.getDefaultToolkit().getImage(url);
 		return image;
+	}
+	
+	public static Image getLogo() {
+		return getImage("instedd_small.png");
 	}
 	
 	public static Image getTableImage() {
-		Image image = Toolkit.getDefaultToolkit().getImage("images/gs_plain.png");
-		return image;
+		return getImage("gs_plain.png");
 	}
 	
 	public static Image getMapImage() {
-		Image image = Toolkit.getDefaultToolkit().getImage("images/kml_plain.png");
-		return image;
+		return getImage("kml_plain.png");
 	}
 	
 	public static Image getFolderImage() {
-		Image image = Toolkit.getDefaultToolkit().getImage("images/zip_plain.png");
-		return image;
+		return getImage("zip_plain.png");
 	}
 
 }
