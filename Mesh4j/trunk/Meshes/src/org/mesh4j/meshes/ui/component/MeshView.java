bfb997eefb5be1a201ad0ef8cacf755ae5eec7f1
juan.wajnerman <juan.wajnerman@8593f39d-8f4b-0410-bdb6-4f53e344ee92>
2010-07-13 15:54:34 +0000

    Meshes: changes in MeshView can now be saved

--- Mesh4j/trunk/Meshes/src/org/mesh4j/meshes/ui/component/MeshView.java
+++ Mesh4j/trunk/Meshes/src/org/mesh4j/meshes/ui/component/MeshView.java
@@ -3,19 +3,23 @@ package org.mesh4j.meshes.ui.component;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
+import java.io.IOException;
 
-import javax.swing.JComponent;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JPasswordField;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
 
+import org.mesh4j.meshes.io.ConfigurationManager;
 import org.mesh4j.meshes.model.Mesh;
+import org.mesh4j.sync.validations.MeshException;
 
 @SuppressWarnings("serial")
-public class MeshView extends JComponent {
+public class MeshView extends EditableComponent {
 
 	private final Mesh mesh;
 	private JTextField nameField;
@@ -56,6 +60,7 @@ public class MeshView extends JComponent {
 
 		descriptionField = new JTextArea(mesh.getDescription(), 3, 30);
 		add(new JScrollPane(descriptionField), c);
+		descriptionField.getDocument().addDocumentListener(new DocumentListenerImplementation());
 		
 		serverUrlField = new JTextField(mesh.getServerFeedUrl(), 50);
 		serverUrlField.setEnabled(false);
@@ -63,11 +68,51 @@ public class MeshView extends JComponent {
 		
 		passwordField = new JPasswordField(mesh.getPassword());
 		add(passwordField, c);
+		passwordField.getDocument().addDocumentListener(new DocumentListenerImplementation());
 		
 		// Fillers
 		c.gridx = 0; c.gridy = 4; c.weightx = 0; c.weighty = 10;
 		add(new JPanel(), c);
 	}
+
+	@Override
+	protected void loadModel() {
+		descriptionField.setText(mesh.getDescription());
+		passwordField.setText(mesh.getPassword());
+	}
+
+	@Override
+	public boolean isDirty() {
+		return !descriptionField.getText().equals(mesh.getDescription()) ||
+			!new String(passwordField.getPassword()).equals(mesh.getPassword());
+	}
+
+	@Override
+	public void saveChanges() {
+		mesh.setDescription(descriptionField.getText());
+		mesh.setPassword(new String(passwordField.getPassword()));
+		try {
+			ConfigurationManager.getInstance().saveMesh(mesh);
+			notifyEditableListener();
+		} catch (IOException e) {
+			throw new MeshException(e);
+		}
+	}
 	
-	
+	private final class DocumentListenerImplementation implements DocumentListener {
+		@Override
+		public void removeUpdate(DocumentEvent e) {
+			notifyEditableListener();
+		}
+
+		@Override
+		public void insertUpdate(DocumentEvent e) {
+			notifyEditableListener();
+		}
+
+		@Override
+		public void changedUpdate(DocumentEvent e) {
+			notifyEditableListener();
+		}
+	}
 }
