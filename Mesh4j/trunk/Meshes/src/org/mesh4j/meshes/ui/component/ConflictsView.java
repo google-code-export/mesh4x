7f8ad9422fe0ab76c496f2a3b86cf324463739f9
juan.wajnerman <juan.wajnerman@8593f39d-8f4b-0410-bdb6-4f53e344ee92>
2010-09-15 20:12:51 +0000

    Meshes: display different versions of a conflicting item.

--- Mesh4j/trunk/Meshes/src/org/mesh4j/meshes/ui/component/ConflictsView.java
+++ Mesh4j/trunk/Meshes/src/org/mesh4j/meshes/ui/component/ConflictsView.java
@@ -8,19 +8,30 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
 import javax.swing.table.DefaultTableModel;
 
 import net.miginfocom.swing.MigLayout;
 
 import org.mesh4j.meshes.model.DataSource;
 import org.mesh4j.sync.ISyncAdapter;
+import org.mesh4j.sync.adapters.split.IContentAdapter;
+import org.mesh4j.sync.adapters.split.IIdentifiableContentAdapter;
+import org.mesh4j.sync.adapters.split.SplitAdapter;
 import org.mesh4j.sync.model.Item;
+import org.mesh4j.sync.payload.schema.ISchema;
 
 @SuppressWarnings("serial")
-public class ConflictsView extends JPanel {
+public class ConflictsView extends JPanel implements ListSelectionListener {
 
 	private DataSource dataSource;
 	private DefaultTableModel conflictsModel;
+	private JPanel versionsPanel;
+	private List<Item> itemsWithConflicts;
+	private ISyncAdapter syncAdapter;
+	private ISchema schema;
+	private JTable conflictTable;
 
 	public ConflictsView(DataSource dataSource) {
 		this.dataSource = dataSource;
@@ -32,18 +43,31 @@ public class ConflictsView extends JPanel {
 		setLayout(new MigLayout("insets 10, fill"));
 
 		add(new JLabel("Items:"), "wrap");
-		JTable conflictTable = new JTable();
+		conflictTable = new JTable();
 		add(new JScrollPane(conflictTable), "growx, wrap");
 		conflictsModel = new DefaultTableModel(new String[] { "Item ID", "# of Conflicting Versions", "Conflicting Users" }, 0);
 		conflictTable.setModel(conflictsModel);
+		conflictTable.getSelectionModel().addListSelectionListener(this);
+		
+		add(new JLabel("Conflicting versions:"), "wrap");
+		add(new JScrollPane(versionsPanel = new JPanel(new MigLayout("insets 0")), JScrollPane.VERTICAL_SCROLLBAR_NEVER, JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS), "growx, height 50%!");
 	}
 	
 	private void loadConflicts() {
 		new Thread(new Runnable() {
+
 			@Override
 			public void run() {
-				ISyncAdapter syncAdapter = dataSource.createSyncAdapter();
-				List<Item> itemsWithConflicts = syncAdapter.getConflicts();
+				syncAdapter = dataSource.createSyncAdapter();
+				
+				if (syncAdapter instanceof SplitAdapter) {
+					IContentAdapter contentAdapter = ((SplitAdapter)syncAdapter).getContentAdapter();
+					if (contentAdapter instanceof IIdentifiableContentAdapter) {
+						schema = ((IIdentifiableContentAdapter) contentAdapter).getSchema();
+					}
+				}
+				
+				itemsWithConflicts = syncAdapter.getConflicts();
 
 				for (Item item : itemsWithConflicts) {
 					Set<String> users = new HashSet<String>();
@@ -66,5 +90,25 @@ public class ConflictsView extends JPanel {
 	}
 	private String getItemAuthor(Item item) {
 		return item.getSync().getUpdatesHistory().peek().getBy();
+	}
+
+	@Override
+	public void valueChanged(ListSelectionEvent e) {
+		if (e.getValueIsAdjusting()) return;
+		Item item = itemsWithConflicts.get(conflictTable.getSelectedRow());
+		versionsPanel.removeAll();
+		
+		ItemView itemView;
+		versionsPanel.add(itemView = new ItemView(item, schema), "width 33%!, growy");
+		versionsPanel.getParent().doLayout();
+		versionsPanel.doLayout();
+		itemView.doLayout();
+		
+		for (Item conflictItem : item.getSync().getConflicts()) {
+			versionsPanel.add(itemView = new ItemView(conflictItem, schema), "width 33%!, growy");
+			versionsPanel.getParent().doLayout();
+			versionsPanel.doLayout();
+			itemView.doLayout();
+		}
 	}
 }
---
+++ Mesh4j/trunk/Meshes/src/org/mesh4j/meshes/ui/component/ItemView.java
@@ -0,0 +1,39 @@
+package org.mesh4j.meshes.ui.component;
+
+import java.util.Map;
+
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+
+import net.miginfocom.swing.MigLayout;
+
+import org.mesh4j.sync.model.Item;
+import org.mesh4j.sync.payload.schema.ISchema;
+
+@SuppressWarnings("serial")
+public class ItemView extends JPanel {
+	
+	private final ISchema schema;
+	private final Item item;
+
+	public ItemView(Item item, ISchema schema) {
+		this.item = item;
+		this.schema = schema;
+		initializeView();
+	}
+
+	private void initializeView() {
+		setLayout(new MigLayout("insets 0"));
+		Map<String, Object> properties = schema.getPropertiesAsMap(item.getContent().getPayload());
+		
+		DefaultTableModel model = new DefaultTableModel(new String[] { "Field", "Value" }, 0);
+		add(new JScrollPane(new JTable(model)), "grow");
+		
+		for (String key : properties.keySet()) {
+			model.addRow(new String[] { key, properties.get(key).toString() });
+		}
+	}
+
+}
